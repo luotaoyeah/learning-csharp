5d9820e0e507bf638e90511ec06e95c665808276
luotao <luotaoyeah@gmail.com>
2019-04-30 14:44:10 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learning_csharp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD");
+        }
+    }
+}
