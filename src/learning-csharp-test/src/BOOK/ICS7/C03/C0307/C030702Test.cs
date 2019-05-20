12a535d5c1d0a70a494f845727fe76f37e4d63d4
luotao <luotaoyeah@gmail.com>
2019-05-20 22:02:36 +0800

    feat(illustrated-c#-7): 03-07-02: System.Console.WriteLine()

---
+++ src/learning-csharp-test/src/BOOK/ICS7/C03/C0307/C030702Test.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7.C03.C0307
+{
+    public class C030702Test
+    {
+        [Fact]
+        public void Fn01()
+        {
+            LT.BOOK.ICS7.C03.C0307.C030702.Fn01();
+        }
+    }
+}
