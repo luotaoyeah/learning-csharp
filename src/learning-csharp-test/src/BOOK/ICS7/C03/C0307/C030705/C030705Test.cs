58d80eac32b169fa54ba59c8833cd98ea74401b0
luotao <luotaoyeah@gmail.com>
2019-05-24 21:44:32 +0800

    feat(illustrated-c#-7): 03-07-05: formatting numeric strings

---
+++ src/learning-csharp-test/src/BOOK/ICS7/C03/C0307/C030705/C030705Test.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7.C03.C0307.C030705
+{
+    public static class C030705Test
+    {
+        [Fact]
+        public static void Fn01()
+        {
+            Assert.Equal("¥500.00", LT.BOOK.ICS7.C03.C0307.C030705.C030705.Fn01());
+        }
+    }
+}
