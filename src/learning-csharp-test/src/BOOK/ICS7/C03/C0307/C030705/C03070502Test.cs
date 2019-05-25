b9643976ec1d769954c6d18ec3cafc474e19b44c
luotao <luotaoyeah@gmail.com>
2019-05-25 12:50:44 +0800

    feat(illustrated-c#-7): 03-07-05-02: the format field

---
+++ src/learning-csharp-test/src/BOOK/ICS7/C03/C0307/C030705/C03070502Test.cs
@@ -0,0 +1,14 @@
+using LT.BOOK.ICS7.C03.C0307.C030705;
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7.C03.C0307.C030705
+{
+    public class C03070502Test
+    {
+        [Fact]
+        public void Fn01()
+        {
+            Assert.Equal("9.00 ", C03070502.Fn01());
+        }
+    }
+}
