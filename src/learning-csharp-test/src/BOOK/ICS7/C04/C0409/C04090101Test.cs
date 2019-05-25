fe17011d2b6e71c50ad920f347a8777e89df8c3d
luotao <luotaoyeah@gmail.com>
2019-05-25 16:06:44 +0800

    feat(illustrated-c#-7): 04-09-01-01: variable initializers

---
+++ src/learning-csharp-test/src/BOOK/ICS7/C04/C0409/C04090101Test.cs
@@ -0,0 +1,14 @@
+using LT.BOOK.ICS7.C04.C0409;
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7.C04.C0409
+{
+    public class C04090101Test
+    {
+        [Fact]
+        public void Fn01()
+        {
+            Assert.Equal(9, C04090101.Fn01());
+        }
+    }
+}
