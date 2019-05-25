237340206fd35c4effe948ad59aebaad951351cf
luotao <luotaoyeah@gmail.com>
2019-05-25 15:48:45 +0800

    feat(illustrated-c#-7): 04-08-02: categorizing the C# types

---
+++ src/learning-csharp-test/src/BOOK/ICS7/C04/C0408/C040802Test.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7.C04.C0408
+{
+    public class C040802Test
+    {
+        [Fact]
+        public void Fn01()
+        {
+        }
+    }
+}
---
+++ src/learning-csharp/src/BOOK/ICS7/C04/C0408/C040802.cs
@@ -0,0 +1,35 @@
+namespace LT.BOOK.ICS7.C04.C0408
+{
+    /// <summary>
+    /// 4.8.2. Categorizing the C# Types
+    /// </summary>
+    public static class C040802
+    {
+        public static void Fn01()
+        {
+            // C# 中的类型, 哪些是 value types, 哪些是 reference types ?
+
+            // VALUE TYPES:
+            //   sbyte/byte
+            //   short/ushort
+            //   int/uint
+            //   long/ulong
+            //   bool
+            //   struct
+            //   enum
+
+            // REFERENCE TYPES:
+            //   float
+            //   double
+            //   char
+            //   decimal
+            //   object
+            //   string
+            //   dynamic
+            //   class
+            //   interface
+            //   delegate
+            //   array
+        }
+    }
+}
