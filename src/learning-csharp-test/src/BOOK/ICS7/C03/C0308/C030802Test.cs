f4ac453392f2e914cfaad7198cde13a3e08fe9bf
luotao <luotaoyeah@gmail.com>
2019-05-25 13:57:56 +0800

    feat(illustrated-c#-7): 03-08-02: documentation comments

---
+++ src/learning-csharp-test/src/BOOK/ICS7/C03/C0308/C030802Test.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7.C03.C0308
+{
+    public class C030802Test
+    {
+        [Fact]
+        public void Fn01()
+        {
+        }
+    }
+}
---
+++ src/learning-csharp/src/BOOK/ICS7/C03/C0308/C030802.cs
@@ -0,0 +1,16 @@
+namespace LT.BOOK.ICS7.C03.C0308
+{
+    /// <summary>
+    /// 3.8.2. Documentation Comments
+    /// </summary>
+    public static class C030802
+    {
+        /// <summary>
+        /// documentation comments use 3 slashes rather than 2,
+        /// they contains XML text that can be used to produce program documentation
+        /// </summary>
+        public static void Fn01()
+        {
+        }
+    }
+}
