a208666c843ad7097a1e290895e9de6c1ae3fe0a
luotao <luotaoyeah@gmail.com>
2019-05-25 16:30:39 +0800

    feat(illustrated-c#-7): 04-09-01-02: automatic initialization

---
+++ src/learning-csharp-test/src/BOOK/ICS7/C04/C0409/C04090102Test.cs
@@ -0,0 +1,14 @@
+using LT.BOOK.ICS7.C04.C0409;
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7.C04.C0409
+{
+    public class C04090102Test
+    {
+        [Fact]
+        public void Fn01()
+        {
+            Assert.Equal(0, C04090102.Fn01());
+        }
+    }
+}
