d1e2c162649c75c90e497887f08356d400cf44e4
luotao <luotaoyeah@gmail.com>
2019-05-25 20:57:37 +0800

    feat(illustrated-c#-7): 05-04-02: methods

---
+++ src/learning-csharp-test/src/BOOK/ICS7/C05/C0504/C050402Test.cs
@@ -0,0 +1,14 @@
+using LT.BOOK.ICS7.C05.C0504;
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7.C05.C0504
+{
+    public class C050402Test
+    {
+        [Fact]
+        public void Fn01()
+        {
+            Assert.Equal(3, new C050402().Add(1, 2));
+        }
+    }
+}
