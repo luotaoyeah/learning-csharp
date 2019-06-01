f5a05061f816769d66cf368193ed4c6d9ce45f5b
luotao <luotaoyeah@gmail.com>
2019-06-01 15:31:58 +0800

    feat(illustrated-c#-7): 05-07: instance members

---
+++ src/learning-csharp-test/src/BOOK/ICS7/C05/C0507/C0507Test.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7.C05.C0507
+{
+    public class C0507Test
+    {
+        [Fact]
+        public void Fn01()
+        {
+            LT.BOOK.ICS7.C05.C0507.C0507 c0507A01 = new LT.BOOK.ICS7.C05.C0507.C0507("BAR");
+            LT.BOOK.ICS7.C05.C0507.C0507 c0507A02 = new LT.BOOK.ICS7.C05.C0507.C0507("BAR");
+
+            Assert.True(c0507A01.Bar == c0507A02.Bar);
+            Assert.True(c0507A01.Bar.Equals(c0507A02.Bar));
+        }
+
+        [Fact]
+        public void Fn02()
+        {
+            LT.BOOK.ICS7.C05.C0507.C0507 c0507A01 = new LT.BOOK.ICS7.C05.C0507.C0507("BAR");
+            LT.BOOK.ICS7.C05.C0507.C0507 c0507A02 = new LT.BOOK.ICS7.C05.C0507.C0507("BAR");
+
+            Assert.True(ReferenceEquals(c0507A01.Bar, c0507A02.Bar));
+        }
+
+        [Fact]
+        public void Fn03()
+        {
+            Assert.Null(LT.BOOK.ICS7.C05.C0507.C0507.Foo);
+        }
+    }
+}
