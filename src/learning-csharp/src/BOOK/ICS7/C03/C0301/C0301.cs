61a0ace93dfde81c139ff31de8978ee90a4b1ba1
luotao <luotaoyeah@gmail.com>
2020-04-03 00:45:32 +0800

    refactor(illustrated-c#-7): 3.1. a simple c# program

--- src/learning-csharp/src/BOOK/ICS7/C03/C0301/C0301.cs
+++ src/learning-csharp/src/BOOK/ICS7/C03/C0301/C0301.cs
@@ -2,7 +2,10 @@ using System;
 
 namespace LT.BOOK.ICS7.C03.C0301
 {
-    public static class C0301
+    /// <summary>
+    /// 3.1. A Simple C# Program
+    /// </summary>
+    public class C0301
     {
         public static void Fn01()
         {
