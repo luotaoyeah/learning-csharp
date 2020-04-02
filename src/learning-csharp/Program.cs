13b241daeba8c8d9c675d1c4caca20c3f49454fa
luotao <luotaoyeah@gmail.com>
2020-04-02 23:54:08 +0800

    chore: netcore 5

--- src/learning-csharp/Program.cs
+++ src/learning-csharp/Program.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using LT.BOOK.ICS7.C03.C0307;
+﻿using LT.BOOK.ICS7.C03.C0307;
 
 namespace LT
 {
-    // ReSharper disable once ClassNeverInstantiated.Global
     internal class Program
     {
         public static void Main()
         {
-            Console.WriteLine("--------------------------------------------------");
             C030704.Fn01();
         }
     }
