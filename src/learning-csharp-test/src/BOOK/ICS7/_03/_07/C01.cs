dba599c98a5ae705576b141007a1fc81a08e53ad
luotao <luotaoyeah@gmail.com>
2020-04-05 19:48:32 +0800

    refactor(illustrated-c#-7): System.Console.Write()

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_03/_07/C01.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._03._07
+{
+    public class C01
+    {
+        [Fact]
+        public void T01()
+        {
+            LT.BOOK.ICS7._03._07.C01.Fn01();
+        }
+    }
+}
