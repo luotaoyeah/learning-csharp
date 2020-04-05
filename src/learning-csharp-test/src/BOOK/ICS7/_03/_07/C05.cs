df6bc672a96385eb3c845d77072684773e145a1f
luotao <luotaoyeah@gmail.com>
2020-04-05 21:09:44 +0800

    refactor(illustrated-c#-7): formatting numeric strings

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_03/_07/C05.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._03._07
+{
+    public class C05
+    {
+        [Fact]
+        public void T01()
+        {
+            Assert.Equal("   ¥500.00", LT.BOOK.ICS7._03._07.C05.Fn01());
+        }
+    }
+}
