df4aa69508a4a62579d59005c38ee45c6c4a958b
luotao <luotaoyeah@gmail.com>
2020-04-06 20:32:31 +0800

    feat(illustrated-c#-7): static typing and the dynamic keyword

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_04/_10/C01.cs
@@ -0,0 +1,16 @@
+using LT.BOOK.ICS7._04._10;
+using Microsoft.CSharp.RuntimeBinder;
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._04._10
+{
+    public class C01
+    {
+        [Fact]
+        public void T01()
+        {
+            RuntimeBinderException exception = Assert.Throws<RuntimeBinderException>(() => { C10.Fn01(); });
+            Assert.Equal("Cannot implicitly convert type 'string' to 'int'", exception.Message);
+        }
+    }
+}
