02649b0fd0f9c07e6335643661487f4a048bb78a
luotao <luotaoyeah@gmail.com>
2020-04-07 21:35:05 +0800

    feat(illustrated-c#-7): 5.10 accessing members from outside the class

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_05/_10/C01.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._05._10
+{
+    public class C01
+    {
+        [Fact]
+        public void T01()
+        {
+            var c01 = new LT.BOOK.ICS7._05._10.C01();
+            Assert.Equal(6, c01.X);
+
+            c01.X = 8;
+            Assert.Equal(8, c01.X);
+        }
+    }
+}
