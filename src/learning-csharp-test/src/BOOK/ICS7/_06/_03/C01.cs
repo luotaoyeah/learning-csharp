dcab392a7f8d619d0d259551b1adffb0c3b10364
luotao <luotaoyeah@gmail.com>
2020-04-08 21:30:04 +0800

    feat(illustrated-c#-7): 6.3.1 type inference and the var keyword

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_06/_03/C01.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._06._03
+{
+    public class C01
+    {
+        [Fact]
+        public void T01()
+        {
+            Assert.Equal(6, LT.BOOK.ICS7._06._03.C01.Fn01());
+        }
+    }
+}
