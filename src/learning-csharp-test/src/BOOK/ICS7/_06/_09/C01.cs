e54859f10741259a47a3054012c80515945f9343
luotao <luotaoyeah@gmail.com>
2020-04-08 22:50:04 +0800

    feat(illustrated-c#-7): 6.9 local functions

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_06/_09/C01.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._06._09
+{
+    public class C01
+    {
+        [Fact]
+        public void T01()
+        {
+            Assert.Equal(6, LT.BOOK.ICS7._06._09.C01.Fn01(1, 2));
+        }
+    }
+}
