38e279a1fcbfa4303587d5061e1452073f8ddc15
luotao <luotaoyeah@gmail.com>
2020-04-09 20:17:10 +0800

    feat(illustrated-c#-7): 6.13 reference types as value and reference parameters

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_06/_13/C01.cs
@@ -0,0 +1,20 @@
+using LT.BOOK.ICS7._06._13;
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._06._13
+{
+    public class C01
+    {
+        [Fact]
+        public void T01()
+        {
+            var person01 = new Person();
+            LT.BOOK.ICS7._06._13.C01.Fn01(person01);
+            Assert.Equal(0, person01.Age);
+
+            var person02 = new Person();
+            LT.BOOK.ICS7._06._13.C01.Fn02(ref person02);
+            Assert.Equal(18, person02.Age);
+        }
+    }
+}
