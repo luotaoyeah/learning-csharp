bd4d317bc31967e288b28fc619a9584b00af9da1
luotao <luotaoyeah@gmail.com>
2020-04-10 20:52:23 +0800

    feat(illustrated-c#-7): 6.19 named parameters

---
+++ src/learning-csharp/src/BOOK/ICS7/_06/_19/C01.cs
@@ -0,0 +1,13 @@
+namespace LT.BOOK.ICS7._06._19
+{
+    /// <summary>
+    /// 6.19 named parameters
+    /// </summary>
+    public class C01
+    {
+        public static int Fn01(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+    }
+}
