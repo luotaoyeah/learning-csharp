c1c534a138e4a2dc362c0032a52978f30fcb4191
luotao <luotaoyeah@gmail.com>
2020-04-11 22:15:05 +0800

    feat(illustrated-c#-7): 7.8 member constants

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_07/_08/C01.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._07._08
+{
+    public class C01
+    {
+        [Fact]
+        public void T01()
+        {
+            Assert.Equal(3.14, LT.BOOK.ICS7._07._08.C01.Pi);
+        }
+    }
+}
