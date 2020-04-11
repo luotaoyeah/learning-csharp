7e80223fd711fa3f23b5a0424b7f802587fe2afd
luotao <luotaoyeah@gmail.com>
2020-04-11 22:08:41 +0800

    feat(illustrated-c#-7): 7.6 static function members

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_07/_06/C01.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._07._06
+{
+    public class C01
+    {
+        [Fact]
+        public void T01()
+        {
+            Assert.Equal(8, LT.BOOK.ICS7._07._06.C01.Fn01());
+        }
+    }
+}
