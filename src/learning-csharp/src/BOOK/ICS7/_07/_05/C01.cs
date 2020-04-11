f4cdc607affe20c0896bca818fa5841ddd11ef6d
luotao <luotaoyeah@gmail.com>
2020-04-11 21:39:29 +0800

    feat(illustrated-c#-7): accessing static members from outside the class

---
+++ src/learning-csharp/src/BOOK/ICS7/_07/_05/C01.cs
@@ -0,0 +1,10 @@
+namespace LT.BOOK.ICS7._07._05
+{
+    /// <summary>
+    /// 7.5 accessing static members from outside the class
+    /// </summary>
+    public class C01
+    {
+        public static int I01 = 6;
+    }
+}
