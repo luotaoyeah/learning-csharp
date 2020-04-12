7457c12516690d6219776ff5655c2234dbd3aea5
luotao <luotaoyeah@gmail.com>
2020-04-12 21:57:25 +0800

    feat(illustrated-c#-7): 7.12 static constructors

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_07/_12/C01.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._07._12
+{
+    public class C01
+    {
+        [Fact]
+        public void T01()
+        {
+            Assert.Equal(2, LT.BOOK.ICS7._07._12.C01.age);
+        }
+    }
+}
---
+++ src/learning-csharp/src/BOOK/ICS7/_07/_12/C01.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace LT.BOOK.ICS7._07._12
+{
+    /// <summary>
+    /// 7.12 static constructors
+    /// </summary>
+    public class C01
+    {
+        public static int age;
+
+        //----------------------------------------------------------------------------------------------------
+        // static constructor 的特点：
+        //   1. 使用 static 关键字，
+        //   2. 不能定义 parameter，
+        //   3. 不能使用 access modifier，
+        //
+        // 不能在程序中调用 static constructor，它由系统自动调用，
+        // 通常在 static constructor 中初始化 static field 等数据，
+        //----------------------------------------------------------------------------------------------------
+        static C01()
+        {
+            age = 2;
+            Console.Write("222");
+        }
+    }
+}
