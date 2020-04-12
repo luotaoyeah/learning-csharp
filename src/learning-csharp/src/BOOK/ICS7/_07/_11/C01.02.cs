ad411c0e8f43341fe1f10252e8fe4225a7711eb6
luotao <luotaoyeah@gmail.com>
2020-04-12 21:30:31 +0800

    feat(illustrated-c#-7): 7.11.2 default constructors

---
+++ src/learning-csharp/src/BOOK/ICS7/_07/_11/C01.02.cs
@@ -0,0 +1,14 @@
+namespace LT.BOOK.ICS7._07._11
+{
+    /// <summary>
+    /// 7.11.2 default constructors
+    /// </summary>
+    public class C0102
+    {
+        //----------------------------------------------------------------------------------------------------
+        // 如果没有显式地定义 constructor，则 compiler 会隐式地给我们定义一个 default constructor，
+        // 这个 default constructor 没有 parameter，没有 body，
+        // 注意，只要我们显式地定义了任何的 constructor，compiler 都不会再给我们定义 default constructor，
+        //----------------------------------------------------------------------------------------------------
+    }
+}
