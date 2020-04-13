7c6d446d9f2a27a6f38debde874ffbf245a2f37a
luotao <luotaoyeah@gmail.com>
2020-04-13 21:31:24 +0800

    feat(illustrated-c#-7): 7.16.4 declaring an indexer

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_07/_16/C01.04.cs
@@ -0,0 +1,34 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._07._16
+{
+    public class C0104
+    {
+        [Fact]
+        public void T01()
+        {
+            var c0104 = new LT.BOOK.ICS7._07._16.C0104();
+
+            Assert.Equal(1, c0104.X);
+            Assert.Equal(2, c0104.Y);
+            Assert.Equal(3, c0104.Z);
+
+            Assert.Equal(1, c0104[0]);
+            Assert.Equal(2, c0104[1]);
+            Assert.Equal(3, c0104[2]);
+        }
+
+        [Fact]
+        public void T02()
+        {
+            //----------------------------------------------------------------------------------------------------
+            // indexer 也可以使用 object initializer 的形式
+            //----------------------------------------------------------------------------------------------------
+            var c0104 = new LT.BOOK.ICS7._07._16.C0104 { [1] = 6 };
+            c0104[2] = 8;
+
+            Assert.Equal(6, c0104[1]);
+            Assert.Equal(8, c0104[2]);
+        }
+    }
+}
