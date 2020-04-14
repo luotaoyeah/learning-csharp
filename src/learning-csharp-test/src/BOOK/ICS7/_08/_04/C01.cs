3516169b29c4b13aae9af628eded9e02d60bc4bb
luotao <luotaoyeah@gmail.com>
2020-04-14 21:44:24 +0800

    feat(illustrated-c#-7): 8.4 masking members of a base class

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_08/_04/C01.cs
@@ -0,0 +1,16 @@
+using LT.BOOK.ICS7._08._04;
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._08._04
+{
+    public class C01
+    {
+        [Fact]
+        public void T01()
+        {
+            var c02 = new C02();
+            Assert.Equal("8", C02.I01);
+            Assert.Equal(3, c02.Fn01(1));
+        }
+    }
+}
