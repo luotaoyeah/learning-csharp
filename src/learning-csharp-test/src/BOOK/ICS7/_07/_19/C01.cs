67418556835d8b360f8091fcb3ef2047fa0569ca
luotao <luotaoyeah@gmail.com>
2020-04-14 20:24:18 +0800

    feat(illustrated-c#-7): 7.19 partial methods

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_07/_19/C01.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._07._19
+{
+    public class C01
+    {
+        [Fact]
+        public void T01()
+        {
+            Assert.Equal(6, LT.BOOK.ICS7._07._19.C01.Fn01());
+        }
+    }
+}
