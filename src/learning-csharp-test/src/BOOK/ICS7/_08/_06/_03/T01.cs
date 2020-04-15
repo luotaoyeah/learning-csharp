3ca859532ae08fc9e6209a9b6367fb5d543bfb3d
luotao <luotaoyeah@gmail.com>
2020-04-15 21:38:10 +0800

    feat(illustrated-c#-7): 8.6.3 overriding other member types

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_08/_06/_03/T01.cs
@@ -0,0 +1,21 @@
+using LT.BOOK.ICS7._08._06._03;
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._08._06._03
+{
+    /// <summary>
+    /// 8.6.3 overriding other member types
+    /// </summary>
+    public class T01
+    {
+        [Fact]
+        public void _01()
+        {
+            var myDerivedClass = new MyDerivedClass();
+            MyBaseClass myBaseClass = myDerivedClass;
+
+            Assert.Equal(2, myBaseClass.P01);
+            Assert.Equal(2, myBaseClass[0]);
+        }
+    }
+}
---
+++ src/learning-csharp/src/BOOK/ICS7/_08/_06/_03/MyBaseClass.cs
@@ -0,0 +1,13 @@
+namespace LT.BOOK.ICS7._08._06._03
+{
+    public class MyBaseClass
+    {
+        //----------------------------------------------------------------------------------------------------
+        // 除了 method 可以使用 virtual/override 进行覆写之外，property/indexer/event 也可以，
+        //----------------------------------------------------------------------------------------------------
+
+        public virtual int P01 => 1;
+
+        public virtual int this[int index] => 1;
+    }
+}
