de0d76679845c6bcad3911949abfe5e1b97a2965
luotao <luotaoyeah@gmail.com>
2020-04-16 21:23:31 +0800

    feat(illustrated-c#-7): 8.7 constructor execution

---
+++ src/learning-csharp/src/BOOK/ICS7/_08/_07/MyBaseClass.cs
@@ -0,0 +1,9 @@
+namespace LT.BOOK.ICS7._08._07
+{
+    public class MyBaseClass
+    {
+        public MyBaseClass()
+        {
+        }
+    }
+}
---
+++ src/learning-csharp/src/BOOK/ICS7/_08/_07/MyDerivedClass.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace LT.BOOK.ICS7._08._07
+{
+    public class MyDerivedClass : MyBaseClass
+    {
+        public int MyField1 = 5;
+        public int MyField2;
+
+        public MyDerivedClass()
+        {
+        }
+    }
+}
