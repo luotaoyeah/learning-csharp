c9b65c477609cda47d85a8777e70b373a6ce61a1
luotao <luotaoyeah@gmail.com>
2020-04-16 23:08:13 +0800

    feat(illustrated-c#-7): 8.9.4 protected member accessibility

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_08/_09/_04/T01.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._08._09._04
+{
+    /// <summary>
+    /// 8.9.4 protected member accessibility
+    /// </summary>
+    public class T01
+    {
+        [Fact]
+        public void _01()
+        {
+            var myDerivedClass = new LT.BOOK.ICS7._08._09._04.MyDerivedClass();
+            Assert.Equal(1, myDerivedClass.Fn01());
+        }
+    }
+}
---
+++ src/learning-csharp/src/BOOK/ICS7/_08/_09/_04/MyDerivedClass.cs
@@ -0,0 +1,10 @@
+namespace LT.BOOK.ICS7._08._09._04
+{
+    public class MyDerivedClass : MyClass
+    {
+        public int Fn01()
+        {
+            return F01;
+        }
+    }
+}
