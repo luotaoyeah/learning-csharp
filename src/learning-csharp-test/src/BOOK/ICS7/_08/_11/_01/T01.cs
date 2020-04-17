64a60eec3142f95c9e872f77c01e8599c8bb4fec
luotao <luotaoyeah@gmail.com>
2020-04-17 19:47:21 +0800

    feat(illustrated-c#-7): 8.11.1 example of an abstract class and an abstract method

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_08/_11/_01/T01.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._08._11._01
+{
+    /// <summary>
+    /// 8.11.1 example of an abstract class and an abstract method
+    /// </summary>
+    public class T01
+    {
+        [Fact]
+        public void _01()
+        {
+            var derivedClass = new LT.BOOK.ICS7._08._11._01.DerivedClass();
+            Assert.Equal("I am AbClass", derivedClass.IdentifyBase());
+            Assert.Equal("I am DerivedClass", derivedClass.IdentifyDerived());
+        }
+    }
+}
