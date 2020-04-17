69d680bb495a84332037d268b3969b616507c13a
luotao <luotaoyeah@gmail.com>
2020-04-17 20:37:04 +0800

    feat(illustrated-c#-7): 8.13 static classes

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_08/_13/T01.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._08._13
+{
+    /// <summary>
+    /// 8.13 static classes
+    /// </summary>
+    public class T01
+    {
+        [Fact]
+        public void _01()
+        {
+            Assert.Equal(3.14f, LT.BOOK.ICS7._08._13.MyMath.PI);
+        }
+    }
+}
---
+++ src/learning-csharp/src/BOOK/ICS7/_08/_13/MyMath.cs
@@ -0,0 +1,11 @@
+namespace LT.BOOK.ICS7._08._13
+{
+    //----------------------------------------------------------------------------------------------------
+    // static class 的所有 member 都必须是 static member，
+    // static class 可以定义 static constructor，但是不能定义 instance constructor，因此不能创建实例，
+    //----------------------------------------------------------------------------------------------------
+    public static class MyMath
+    {
+        public static float PI = 3.14f;
+    }
+}
