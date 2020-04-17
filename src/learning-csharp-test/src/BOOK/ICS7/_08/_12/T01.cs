83fd83ba51c9c06cd71348b701567c482f909926
luotao <luotaoyeah@gmail.com>
2020-04-17 20:12:53 +0800

    feat(illustrated-c#-7): 8.12 sealed classes

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_08/_12/T01.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace LT.TEST.BOOK.ICS7._08._12
+{
+    /// <summary>
+    /// 8.12 sealed classes
+    /// </summary>
+    public class T01
+    {
+        [Fact]
+        public void _01()
+        {
+            var myClass = new LT.BOOK.ICS7._08._12.MyClass();
+            Assert.Equal(6, myClass.Fn01());
+        }
+    }
+}
