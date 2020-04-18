6b63c3892830bfb502067d4ae77310f2cbb1b3a4
luotao <luotaoyeah@gmail.com>
2020-04-18 11:21:15 +0800

    feat(illustrated-c#-7): 9.14 user-defined type conversions

---
+++ src/learning-csharp/src/BOOK/ICS7/_09/_14/LimitedInt.cs
@@ -0,0 +1,44 @@
+namespace LT.BOOK.ICS7._09._14
+{
+    public class LimitedInt
+    {
+        private const int MaxValue = 100;
+        private const int MinValue = 0;
+
+        private int theValue;
+
+        public int TheValue
+        {
+            get => theValue;
+            set
+            {
+                if (value < MinValue)
+                {
+                    theValue = MinValue;
+                }
+                else if (value > MaxValue)
+                {
+                    theValue = MaxValue;
+                }
+                else
+                {
+                    theValue = value;
+                }
+            }
+        }
+
+        //----------------------------------------------------------------------------------------------------
+        // 自定义隐式类型转换的转换方式
+        //----------------------------------------------------------------------------------------------------
+        public static implicit operator int(LimitedInt limitedInt)
+        {
+            return limitedInt.theValue;
+        }
+
+        public static implicit operator LimitedInt(int i)
+        {
+            var limitedInt = new LimitedInt { TheValue = i };
+            return limitedInt;
+        }
+    }
+}
