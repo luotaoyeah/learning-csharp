39918f155fc1a503595838e806f20e10975ddd06
luotao <luotaoyeah@gmail.com>
2020-04-18 12:27:23 +0800

    feat(illustrated-c#-7): 9.16 the typeof operator

---
+++ src/learning-csharp/src/BOOK/ICS7/_09/_16/SomeClass.cs
@@ -0,0 +1,17 @@
+namespace LT.BOOK.ICS7._09._16
+{
+    public class SomeClass
+    {
+        public int X;
+        public int Y;
+
+        public void Fn01()
+        {
+        }
+
+        public int Fn02()
+        {
+            return 0;
+        }
+    }
+}
