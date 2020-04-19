47794e9468d451711ea3b3dc5fb9058d43c75f16
luotao <luotaoyeah@gmail.com>
2020-04-19 20:56:13 +0800

    feat(illustrated-c#-7): 12.1 enumerations

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_12/_01/T01.cs
@@ -0,0 +1,37 @@
+using LT.BOOK.ICS7._12._01;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._12._01
+{
+    /// <summary>
+    /// 12.1 enumerations
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void _01()
+        {
+            Assert.Equal(0, (int) TrafficLight.Green);
+            Assert.Equal(1, (int) TrafficLight.Yellow);
+            Assert.Equal(2, (int) TrafficLight.Red);
+        }
+
+        [Fact]
+        public void _02()
+        {
+            //----------------------------------------------------------------------------------------------------
+            // 可以定义 enum 类型的变量，它的值只能是该 enum 的某个 member，
+            //----------------------------------------------------------------------------------------------------
+            TrafficLight trafficLight = TrafficLight.Yellow;
+            Assert.Equal(1, (int) trafficLight);
+        }
+    }
+}
