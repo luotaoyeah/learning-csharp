052f55cb1e231bfb9542b7086ff2745231a94854
luotao <luotaoyeah@gmail.com>
2020-04-22 21:10:31 +0800

    feat(illustrated-c#-7): 13.7.1 explicit initialization of one-dimensional arrays

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_13/_07/_01/T01.cs
@@ -0,0 +1,42 @@
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._13._07._01
+{
+    /// <summary>
+    /// 13.7.1 explicit initialization of one-dimensional arrays
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void _01()
+        {
+            //----------------------------------------------------------------------------------------------------
+            // 使用 initialization list 初始化一个 array，
+            //----------------------------------------------------------------------------------------------------
+            int[] arr01 = new int[3] { 1, 2, 3 };
+            Assert.Equal(1, arr01[0]);
+            Assert.Equal(2, arr01[1]);
+            Assert.Equal(3, arr01[2]);
+        }
+
+        [Fact]
+        public void _02()
+        {
+            //----------------------------------------------------------------------------------------------------
+            // dimension length 可以省略，因为可以从 initialization list 中元素的个数来推断，
+            //----------------------------------------------------------------------------------------------------
+            int[] arr01 = new int[] { 1, 2, 3 };
+            Assert.Equal(1, arr01[0]);
+            Assert.Equal(2, arr01[1]);
+            Assert.Equal(3, arr01[2]);
+        }
+    }
+}
