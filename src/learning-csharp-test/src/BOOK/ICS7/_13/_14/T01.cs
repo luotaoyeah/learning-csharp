eefb9c1e9c8e60545a4ee122b0fdd626c2057105
luotao <luotaoyeah@gmail.com>
2020-04-25 11:47:17 +0800

    feat(illustrated-c#-7): 13.14 arrays and ref return and ref local

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_13/_14/T01.cs
@@ -0,0 +1,30 @@
+using LT.BOOK.ICS7._13._14;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._13._14
+{
+    /// <summary>
+    /// 13.14 arrays and ref return and ref local
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void _01()
+        {
+            int[] arr01 = { 5, 80 };
+            ref int highest = ref C01.PointerToHighestPositive(arr01);
+            Assert.Equal(80, highest);
+
+            highest = 0;
+            Assert.Equal(0, arr01[1]);
+        }
+    }
+}
