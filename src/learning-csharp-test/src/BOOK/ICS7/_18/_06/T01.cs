883b120ca2b4719a76239197eaafc671213ce49e
luotao <luotaoyeah@gmail.com>
2020-04-28 21:32:34 +0800

    feat(illustrated-c#-7): 18.6 extension methods with generic classes

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_18/_06/T01.cs
@@ -0,0 +1,29 @@
+using LT.BOOK.ICS7._18._06;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._18._06
+{
+    /// <summary>
+    /// 18.6 extension methods with generic classes
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+
+        [Fact]
+        public void _01()
+        {
+            Assert.Equal(1, new Holder().F01(1));
+
+            Assert.Equal(1, new Holder().F02(1));
+            Assert.Equal(3.14, new Holder().F02(3.14));
+        }
+    }
+}
