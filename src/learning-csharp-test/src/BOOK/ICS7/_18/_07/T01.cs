34d59a99422292a65167afd3783798f7c2aba370
luotao <luotaoyeah@gmail.com>
2020-04-28 21:46:38 +0800

    feat(illustrated-c#-7): 18.7 generic structs

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_18/_07/T01.cs
@@ -0,0 +1,27 @@
+using LT.BOOK.ICS7._18._07;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._18._07
+{
+    /// <summary>
+    /// 18.7 generic structs
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+
+        [Fact]
+        public void _01()
+        {
+            var pieceOfData = new PieceOfData<int>(3);
+            Assert.Equal(3, pieceOfData.Data);
+        }
+    }
+}
