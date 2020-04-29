b092f30c1995a6ea5ff726b41649bf41c512f141
luotao <luotaoyeah@gmail.com>
2020-04-29 20:22:39 +0800

    feat(illustrated-c#-7): 18.9.1 an example using generic interfaces

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_18/_09/_01/T01.cs
@@ -0,0 +1,28 @@
+using LT.BOOK.ICS7._18._09._01;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._18._09._01
+{
+    /// <summary>
+    /// 18.9.1 an example using generic interfaces
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+
+        [Fact]
+        public void _01()
+        {
+            var simple = new Simple();
+            Assert.Equal(1, simple.ReturnIt(1));
+            Assert.Equal("1", simple.ReturnIt("1"));
+        }
+    }
+}
---
+++ src/learning-csharp/src/BOOK/ICS7/_18/_09/_01/IMyInterface.cs
@@ -0,0 +1,7 @@
+namespace LT.BOOK.ICS7._18._09._01
+{
+    public interface IMyInterface<T>
+    {
+        public T ReturnIt(T t);
+    }
+}
