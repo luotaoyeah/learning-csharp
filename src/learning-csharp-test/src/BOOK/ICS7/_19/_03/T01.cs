c067ce52c729a68b7a3d2323b34831a388fd318a
luotao <luotaoyeah@gmail.com>
2020-04-29 21:27:21 +0800

    feat(illustrated-c#-7): 19.3 the IEnumerable interface

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_19/_03/T01.cs
@@ -0,0 +1,30 @@
+using LT.BOOK.ICS7._19._03;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._19._03
+{
+    /// <summary>
+    /// 19.3 the IEnumerable interface
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+
+        [Fact]
+        public void _01()
+        {
+            var spectrum = new Spectrum();
+            foreach (string o in spectrum)
+            {
+                testOutputHelper.WriteLine(o);
+            }
+        }
+    }
+}
