40116dcca730d70d5c188af83202c772df3bed07
luotao <luotaoyeah@gmail.com>
2020-04-30 20:34:46 +0800

    feat(illustrated-c#-7): 19.5.1 iterator blocks

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_19/_05/_01/T01.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using LT.BOOK.ICS7._19._05._01;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._19._05._01
+{
+    /// <summary>
+    /// 19.5.1 iterator blocks
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+
+        [Fact]
+        public void _01()
+        {
+            IEnumerator<string> enumerator = C01.BlackAndWhite();
+
+            if (enumerator.MoveNext())
+            {
+                Assert.Equal("black", enumerator.Current);
+            }
+
+            if (enumerator.MoveNext())
+            {
+                Assert.Equal("gray", enumerator.Current);
+            }
+
+            if (enumerator.MoveNext())
+            {
+                Assert.Equal("white", enumerator.Current);
+            }
+
+            Assert.False(enumerator.MoveNext());
+        }
+    }
+}
