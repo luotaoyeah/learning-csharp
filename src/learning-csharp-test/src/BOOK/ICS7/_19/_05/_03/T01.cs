6572a18a19a8a13ce14fd858bfa3297cbbdcdb43
luotao <luotaoyeah@gmail.com>
2020-04-30 20:43:49 +0800

    feat(illustrated-c#-7): 19.5.3 using an iterator to create an enumerable

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_19/_05/_03/T01.cs
@@ -0,0 +1,31 @@
+using LT.BOOK.ICS7._19._05._03;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._19._05._03
+{
+    /// <summary>
+    /// 19.5.3 using an iterator to create an enumerable
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+
+        [Fact]
+        public void _01()
+        {
+            var myClass = new MyClass();
+
+            foreach (var s in myClass)
+            {
+                testOutputHelper.WriteLine(s);
+            }
+        }
+    }
+}
