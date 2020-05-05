10f4b21ea86e15d499b45bf97243cf13d3593e76
luotao <luotaoyeah@gmail.com>
2020-05-05 20:21:44 +0800

    feat(illustrated-c#-7): 20.6.1 signatures of the standard query operators

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_20/_06/_01/T01.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._20._06._01
+{
+    /// <summary>
+    /// 20.6.1 signatures of the standard query operators
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+
+        /// <summary>
+        /// standard query operators 都是一些 extension methods，
+        /// extension method 可以当初普通方法来调用，
+        /// </summary>
+        [Fact]
+        public void _01()
+        {
+            int[] arr01 = { 2, 4, 6, 8 };
+
+            int sum01 = arr01.Sum();
+            Assert.Equal(20, sum01);
+
+            int sum02 = Enumerable.Sum(arr01);
+            Assert.Equal(20, sum02);
+        }
+    }
+}
