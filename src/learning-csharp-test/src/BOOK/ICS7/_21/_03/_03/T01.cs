6e5e8b0a495ab3dcbcec9f3963f49f80d2786f43
luotao <luotaoyeah@gmail.com>
2020-05-07 20:43:35 +0800

    feat(illustrated-c#-7): 21.3.3 waiting synchronously for tasks in the calling method

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_21/_03/_03/T01.cs
@@ -0,0 +1,28 @@
+using LT.BOOK.ICS7._21._03._03;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._21._03._03
+{
+    /// <summary>
+    /// 21.3.3 waiting synchronously for tasks in the calling method
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+
+        [Fact]
+        public void _01()
+        {
+            MyDownloadString myDownloadString =
+                new MyDownloadString(testOutputHelper.WriteLine);
+            myDownloadString.DoRun();
+        }
+    }
+}
