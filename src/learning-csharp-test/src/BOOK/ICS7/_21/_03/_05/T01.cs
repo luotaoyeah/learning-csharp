4312e1beba6b820d438574300dbd326440ad431a
luotao <luotaoyeah@gmail.com>
2020-05-07 20:50:07 +0800

    feat(illustrated-c#-7): 21.3.5 the Task.Delay method

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_21/_03/_05/T01.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using LT.BOOK.ICS7._21._03._05;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._21._03._05
+{
+    /// <summary>
+    /// 21.3.5 the Task.Delay method
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+
+        [Fact]
+        public void _01()
+        {
+            Simple simple = new Simple(testOutputHelper.WriteLine);
+            Task task = simple.ShowDelayAsync();
+            task.Wait();
+        }
+    }
+}
