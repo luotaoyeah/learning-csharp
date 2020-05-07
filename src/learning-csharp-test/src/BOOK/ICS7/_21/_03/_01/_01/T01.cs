024f916ccc69af328a9c2aa5e348c7717c76c15c
luotao <luotaoyeah@gmail.com>
2020-05-07 19:44:36 +0800

    feat(illustrated-c#-7): 21.3.1.1 the await expression

---
+++ src/learning-csharp-test/src/BOOK/ICS7/_21/_03/_01/_01/T01.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using LT.BOOK.ICS7._21._03._01._01;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.ICS7._21._03._01._01
+{
+    /// <summary>
+    /// 21.3.1.1 the await expression
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+
+        [Fact]
+        public async void _01()
+        {
+            Task<int> task = new MyClass().DoWorkAsync();
+            testOutputHelper.WriteLine(task.Result.ToString());
+        }
+    }
+}
