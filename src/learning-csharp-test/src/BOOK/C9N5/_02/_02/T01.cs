f001bf8467a3cb9fbb22d9d34f3c00d69fbbb5a4
luotao <luotaoyeah@gmail.com>
2020-05-08 19:29:09 +0800

    feat(c#-9-net-5): 2.2 understanding c# basics

---
+++ src/learning-csharp-test/src/BOOK/C9N5/_02/_02/T01.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+// 下面的指令会导致编译报错，报错信息中包含 compiler 的版本和 C# 的版本
+// #error version
+
+namespace LT.TEST.BOOK.C9N5._02._02
+{
+    /// <summary>
+    /// 2.2 understanding c# basics
+    /// </summary>
+    public class T01
+    {
+        [Fact]
+        public void _01()
+        {
+        }
+    }
+}
