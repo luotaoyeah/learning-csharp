3bae671ebcafd6201fe22f04bacc62efe2f6c979
luotao <luotaoyeah@gmail.com>
2020-05-10 23:20:03 +0800

    feat(c#-9-net-5): 3.4.6.1 avoiding exceptions using the tryparse method

--- src/learning-csharp-test/src/BOOK/C9N5/_03/_04/_06/T01.cs
+++ src/learning-csharp-test/src/BOOK/C9N5/_03/_04/_06/T01.cs
@@ -35,6 +35,22 @@ namespace LT.TEST.BOOK.C9N5._03._04._06
             Assert.Throws<FormatException>(ParseIntFail);
         }
 
+        /// <summary>
+        /// <see cref="System.Int32.TryParse(string?, out int)"/> 方法转换失败时会返回 false,
+        /// 转化成功时会返回 true, 并将转换结果存到 out 参数中.
+        /// </summary>
+        [Fact]
+        public void _03()
+        {
+            int i01;
+
+            Assert.False(int.TryParse("A", out i01));
+            Assert.Equal(0, i01);
+
+            Assert.True(int.TryParse("6", out i01));
+            Assert.Equal(6, i01);
+        }
+
         private void ParseIntFail()
         {
             int i01 = int.Parse("A");
