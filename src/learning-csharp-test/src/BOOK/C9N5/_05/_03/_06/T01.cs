480803fcefa77181781db90c44b39912742d56f8
luotao <luotaoyeah@gmail.com>
2020-05-13 22:31:08 +0800

    feat(c#-9-net-5): 5.3.6 storing multiple values using collections

---
+++ src/learning-csharp-test/src/BOOK/C9N5/_05/_03/_06/T01.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.C9N5._05._03._06
+{
+    /// <summary>
+    /// 5.3.6 storing multiple values using collections
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void _01()
+        {
+            var person = new Person();
+            person.Children.Add(new Person() { Name = "luotao" });
+            person.Children.Add(new Person() { Name = "owen" });
+
+            Assert.Equal(2, person.Children.Count);
+
+            foreach (var child in person.Children)
+            {
+                testOutputHelper.WriteLine(child.Name);
+            }
+        }
+    }
+
+    public class Person
+    {
+        public List<Person> Children = new List<Person>();
+        public string? Name;
+    }
+}
