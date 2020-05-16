2428490b93c32cefe8751bd324213f13e3266dcf
luotao <luotaoyeah@gmail.com>
2020-05-16 21:18:12 +0800

    feat(c#-9-net-5): 6.7.3 overriding members

--- src/learning-csharp-lib/src/BOOK/ICS7/_08/_06/_03/MyBaseClass.cs
+++ src/learning-csharp-lib/src/BOOK/ICS7/_08/_06/_03/MyBaseClass.cs
@@ -3,7 +3,7 @@ namespace LT.LIB.BOOK.ICS7._08._06._03
     public class MyBaseClass
     {
         //----------------------------------------------------------------------------------------------------
-        // 除了 method 可以使用 virtual/override 进行覆写之外，property/indexer/event 也可以，
+        // 除了 method 可以使用 virtual/override 进行覆写之外, property/indexer/event 也可以.
         //----------------------------------------------------------------------------------------------------
 
         public virtual int P01 => 1;
---
+++ src/learning-csharp-test/src/BOOK/C9N5/_06._07._03/T01.cs
@@ -0,0 +1,53 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.C9N5._06._07._03
+{
+    /// <summary>
+    ///     6.7.3 overriding members
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void _01()
+        {
+            Assert.Equal(1, new C01().F01());
+            Assert.Equal(2, new C02().F01());
+        }
+    }
+
+    public class C01
+    {
+        /// <summary>
+        ///     父类的方法声明为 virtual, 则子类可以 override 该方法.
+        /// </summary>
+        /// <returns></returns>
+        public virtual int F01()
+        {
+            Console.WriteLine($"{nameof(C01)}.{nameof(F01)}()");
+            return 1;
+        }
+    }
+
+    public class C02 : C01
+    {
+        public override int F01()
+        {
+            //----------------------------------------------------------------------------------------------------
+            // 在子类中通过 base 访问父类的成员.
+            //----------------------------------------------------------------------------------------------------
+            base.F01();
+
+            Console.WriteLine($"{nameof(C02)}.{nameof(F01)}()");
+            return 2;
+        }
+    }
+}
