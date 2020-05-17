2bb98a7e290cb2ddb1d7dcf2712bd5233be3b5d7
luotao <luotaoyeah@gmail.com>
2020-05-17 23:39:45 +0800

    feat(c#-9-net-5): 8.7.3 creating custom attributes

---
+++ src/learning-csharp-test/src/BOOK/C9N5/_08._07._03/T01.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Reflection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LT.TEST.BOOK.C9N5._08._07._03
+{
+    /// <summary>
+    ///     8.7.3 creating custom attributes
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void _01()
+        {
+            var type = typeof(C01);
+            var customAttributes = type.GetCustomAttributes<CoderAttribute>();
+            foreach (var customAttribute in customAttributes) testOutputHelper.WriteLine(customAttribute.Coder);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class CoderAttribute : Attribute
+    {
+        public string? Coder;
+
+        public CoderAttribute(string? coder)
+        {
+            Coder = coder;
+        }
+    }
+
+    [Coder("luotao")]
+    [Coder("owen")]
+    public class C01
+    {
+        [Coder("luotao")]
+        public static void F01()
+        {
+        }
+    }
+}
