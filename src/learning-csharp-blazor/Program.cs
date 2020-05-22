d12731192920b95d83df45b756b17c404da0f499
luotao <luotaoyeah@gmail.com>
2020-05-22 22:49:02 +0800

    chore: adopt stylecop

--- src/learning-csharp-blazor/Program.cs
+++ src/learning-csharp-blazor/Program.cs
@@ -1,21 +1,29 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Luotao.Blazor
 {
-  public class Program
-  {
-    public static async Task Main(string[] args)
+    /// <summary>
+    /// Program.
+    /// </summary>
+    public static class Program
     {
-      var builder = WebAssemblyHostBuilder.CreateDefault(args);
-      builder.RootComponents.Add<App>("#app");
+        /// <summary>
+        /// Main.
+        /// </summary>
+        /// <param name="args">args.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-      builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-      await builder.Build().RunAsync();
+            return builder.Build().RunAsync();
+        }
     }
-  }
 }
