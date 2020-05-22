ff56445bbcbbe85f35e4f312c42aa79559e78a27
luotao <luotaoyeah@gmail.com>
2020-05-22 22:46:28 +0800

    chore: adopt .editorconfig

--- src/learning-csharp-mvc/Data/ApplicationDbContext.cs
+++ src/learning-csharp-mvc/Data/ApplicationDbContext.cs
@@ -3,11 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Luotao.Mvc.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+  public class ApplicationDbContext : IdentityDbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
     }
+  }
 }
