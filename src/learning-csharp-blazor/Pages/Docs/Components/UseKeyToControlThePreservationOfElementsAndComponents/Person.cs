fbd6ee522f1ebf1d7f6b4d0251d8d0d98c6f1874
luotao <luotaoyeah@gmail.com>
2020-05-27 21:50:32 +0800

    feat: https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#use-key-to-control-the-preservation-of-elements-and-components

---
+++ src/learning-csharp-blazor/Pages/Docs/Components/UseKeyToControlThePreservationOfElementsAndComponents/Person.cs
@@ -0,0 +1,13 @@
+namespace Luotao.Blazor.Pages.Docs.Components.UseKeyToControlThePreservationOfElementsAndComponents
+{
+    /// <summary>
+    /// Person.
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Data.
+        /// </summary>
+        public string Data { get; set; }
+    }
+}
---
+++ src/learning-csharp-blazor/Pages/Docs/Components/UseKeyToControlThePreservationOfElementsAndComponents/UseKeyToControlThePreservationOfElementsAndComponents.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Luotao.Blazor.Pages.Docs.Components.UseKeyToControlThePreservationOfElementsAndComponents
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#use-key-to-control-the-preservation-of-elements-and-components.
+    /// </summary>
+    [Route("/docs/components/use-key-to-control-the-preservation-of-elements-and-components")]
+    public partial class UseKeyToControlThePreservationOfElementsAndComponents
+    {
+    }
+}
