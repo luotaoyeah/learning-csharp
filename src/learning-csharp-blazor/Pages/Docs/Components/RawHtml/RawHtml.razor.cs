0f2b87470d7deeb361b1e06ccb6ec0605f2c2077
luotao <luotaoyeah@gmail.com>
2020-05-27 22:14:43 +0800

    feat: https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#raw-html

---
+++ src/learning-csharp-blazor/Pages/Docs/Components/RawHtml/RawHtml.razor.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Luotao.Blazor.Pages.Docs.Components.RawHtml
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#raw-html.
+    /// </summary>
+    [Route("/docs/components/raw-html")]
+    public sealed partial class RawHtml
+    {
+        private readonly string html = "<div style=\"color:red;\">HTML</div>";
+    }
+}
