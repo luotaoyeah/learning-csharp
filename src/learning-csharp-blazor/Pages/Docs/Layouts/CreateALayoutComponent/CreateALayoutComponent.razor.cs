0db5832c7e534d66376d9e248797caf5c2f4bd59
luotao <luotaoyeah@gmail.com>
2020-05-28 21:59:15 +0800

    feat: https://docs.microsoft.com/en-us/aspnet/core/blazor/components/layouts?view=aspnetcore-5.0#create-a-layout-component

---
+++ src/learning-csharp-blazor/Pages/Docs/Layouts/CreateALayoutComponent/CreateALayoutComponent.razor.cs
@@ -0,0 +1,14 @@
+using Luotao.Blazor.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace Luotao.Blazor.Pages.Docs.Layouts.CreateALayoutComponent
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/layouts?view=aspnetcore-5.0#create-a-layout-component.
+    /// </summary>
+    [Layout(typeof(Layout01))]
+    [Route("/docs/components/layouts/create-a-layout-component")]
+    public partial class CreateALayoutComponent
+    {
+    }
+}
---
+++ src/learning-csharp-blazor/Shared/Layout01.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Luotao.Blazor.Shared
+{
+    /// <summary>
+    /// Layout01.
+    /// </summary>
+    public partial class Layout01 : LayoutComponentBase
+    {
+    }
+}
