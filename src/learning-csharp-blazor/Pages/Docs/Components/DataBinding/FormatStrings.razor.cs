9b9c717ce9835cec0766bbb1b9e58eecf06ce904
luotao <luotaoyeah@gmail.com>
2020-05-29 22:30:07 +0800

    feat: https://docs.microsoft.com/en-us/aspnet/core/blazor/components/data-binding?view=aspnetcore-5.0#format-strings

---
+++ src/learning-csharp-blazor/Pages/Docs/Components/DataBinding/FormatStrings.razor.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Luotao.Blazor.Pages.Docs.Components.DataBinding
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/data-binding?view=aspnetcore-5.0#format-strings.
+    /// </summary>
+    [Route("/docs/components/data-binding/format-strings")]
+    public partial class FormatStrings
+    {
+        private DateTime? inputValue = DateTime.Now;
+    }
+}
