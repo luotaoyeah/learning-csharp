5d32781d3f0e3bac2b95b0143b3a9d1523267a7f
luotao <luotaoyeah@gmail.com>
2020-05-30 15:40:01 +0800

    feat: https://docs.microsoft.com/en-us/aspnet/core/blazor/components/event-handling?view=aspnetcore-5.0#eventcallback

---
+++ src/learning-csharp-blazor/Pages/Docs/Components/EventHandling/EventCallback/Child.razor.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Luotao.Blazor.Pages.Docs.Components.EventHandling.EventCallback
+{
+    /// <summary>
+    /// Child.
+    /// </summary>
+    public sealed partial class Child
+    {
+        /// <summary>
+        /// 自定义的事件.
+        /// </summary>
+        [Parameter]
+        public EventCallback<int> MyClick { get; set; }
+
+        private Task OnClick()
+        {
+            return MyClick.InvokeAsync(666);
+        }
+    }
+}
---
+++ src/learning-csharp-blazor/Pages/Docs/Components/EventHandling/EventCallback/EventCallback.razor.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+namespace Luotao.Blazor.Pages.Docs.Components.EventHandling.EventCallback
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/event-handling?view=aspnetcore-5.0#eventcallback.
+    /// </summary>
+    [Route("/docs/components/event-handling/eventcallback")]
+    public sealed partial class EventCallback
+    {
+        [Inject]
+        private ILogger<EventCallback>? Logger { get; set; }
+
+        private void OnClick(int value)
+        {
+            Logger?.LogInformation(value.ToString());
+        }
+    }
+}
