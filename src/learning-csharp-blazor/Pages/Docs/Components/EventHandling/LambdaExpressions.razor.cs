a835f63e7675e61a48af1a744c0f5000cd7fd020
luotao <luotaoyeah@gmail.com>
2020-05-30 15:26:19 +0800

    feat: https://docs.microsoft.com/en-us/aspnet/core/blazor/components/event-handling?view=aspnetcore-5.0#lambda-expressions

---
+++ src/learning-csharp-blazor/Pages/Docs/Components/EventHandling/LambdaExpressions.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Luotao.Blazor.Pages.Docs.Components.EventHandling
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/event-handling?view=aspnetcore-5.0#lambda-expressions.
+    /// </summary>
+    [Route("/docs/components/event-handling/lambda-expressions")]
+    public sealed partial class LambdaExpressions
+    {
+    }
+}
