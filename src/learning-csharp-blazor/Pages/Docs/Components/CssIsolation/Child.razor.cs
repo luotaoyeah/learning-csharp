8d1e179a63fffaf684a8c866a7c30ed0281e3fdc
luotao <luotaoyeah@gmail.com>
2020-06-04 23:34:41 +0800

    feat: https://docs.microsoft.com/en-us/aspnet/core/blazor/components/css-isolation?view=aspnetcore-5.0#child-component-support

---
+++ src/learning-csharp-blazor/Pages/Docs/Components/CssIsolation/Child.razor.cs
@@ -0,0 +1,9 @@
+namespace Luotao.Blazor.Pages.Docs.Components.CssIsolation
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/css-isolation?view=aspnetcore-5.0#child-component-support.
+    /// </summary>
+    public partial class Child
+    {
+    }
+}
