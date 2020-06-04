0181c606767d987b819cfb5c6aabf10d4d0b5004
luotao <luotaoyeah@gmail.com>
2020-06-04 23:14:46 +0800

    feat: https://docs.microsoft.com/en-us/aspnet/core/blazor/components/css-isolation?view=aspnetcore-5.0#enable-css-isolation

---
+++ src/learning-csharp-blazor/Pages/Docs/Components/CssIsolation/CssIsolation.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Luotao.Blazor.Pages.Docs.Components.CssIsolation
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/css-isolation?view=aspnetcore-5.0#enable-css-isolation.
+    /// </summary>
+    [Route("/docs/components/css-isolation")]
+    public partial class CssIsolation
+    {
+    }
+}
