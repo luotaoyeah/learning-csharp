e9978103f30da10f4ec726f35c1da9a4ec7ac8ee
luotao <luotaoyeah@gmail.com>
2020-06-05 23:02:23 +0800

    feat: https://docs.microsoft.com/en-us/aspnet/core/blazor/components/class-libraries?view=aspnetcore-5.0&tabs=visual-studio-code#create-an-rcl-with-static-assets

---
+++ src/learning-csharp-blazor/Pages/Docs/Components/ClassLibraries/ClassLibraries.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Luotao.Blazor.Pages.Docs.Components.ClassLibraries
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/class-libraries?view=aspnetcore-5.0&tabs=visual-studio-code#consume-a-razor-component-from-an-rcl.
+    /// </summary>
+    [Route("/docs/components/class-libraries/consume-a-razor-component-from-an-rcl")]
+    public partial class ClassLibraries
+    {
+    }
+}
