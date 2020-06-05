354d77994c2621b79317924a8e1b8a46091f1a1b
luotao <luotaoyeah@gmail.com>
2020-06-05 23:48:47 +0800

    feat: https://docs.microsoft.com/en-us/aspnet/core/blazor/globalization-localization?view=aspnetcore-5.0#localization

---
+++ src/learning-csharp-blazor/Pages/Docs/GlobalizationLocalization/GlobalizationLocalization.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Luotao.Blazor.Pages.Docs.GlobalizationLocalization
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/globalization-localization?view=aspnetcore-5.0#localization.
+    /// </summary>
+    [Route("/docs/globalization-localization/localization")]
+    public partial class GlobalizationLocalization
+    {
+    }
+}
