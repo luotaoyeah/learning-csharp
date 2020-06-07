d95d50723c457fc1d52c9b56c7fd7d6f14a490c2
luotao <luotaoyeah@gmail.com>
2020-06-07 13:29:46 +0800

    refactor(illustrated-c#-7): 21.1.1 a starting example

---
+++ src/learning-csharp-test/src/Books/ICS7/_21._01._01/MyDownloadStringAsync.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Luotao.Test.Books.ICS7._21._01._01
+{
+  public class MyDownloadStringAsync
+  {
+    private readonly Stopwatch sw = new();
+
+    /// <summary>
+    /// 异步执行.
+    /// </summary>
+    public void DoRun()
+    {
+      const int largeNumber = 6_000_000;
+      sw.Start();
+
+      Task<int> t1 = CountCharactersAsync(1, "http://www.microsoft.com");
+      Task<int> t2 = CountCharactersAsync(2, "http://www.illustratedcsharp.com");
+
+      CountToALargeNumber(1, largeNumber);
+      CountToALargeNumber(2, largeNumber);
+      CountToALargeNumber(3, largeNumber);
+      CountToALargeNumber(4, largeNumber);
+
+      Console.WriteLine($"Chars in http://www.microsoft.com                : {t1.Result,10:N0}");
+      Console.WriteLine($"Chars in http://www.illustratedcsharp.com        : {t2.Result,10:N0}");
+    }
+
+    /// <summary>
+    /// <see cref="Task{T}"/> 类似于 js 中的 Promise.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="uriString"></param>
+    /// <returns></returns>
+    private async Task<int> CountCharactersAsync(int id, string uriString)
+    {
+      WebClient webClient = new();
+      Console.WriteLine($"Starting Call {id} :\t{sw.Elapsed.TotalMilliseconds,10:N0} ms");
+
+      string result = await webClient.DownloadStringTaskAsync(uriString);
+      Console.WriteLine($"Complete Call {id} :\t{sw.Elapsed.TotalMilliseconds,10:N0} ms");
+
+      return result.Length;
+    }
+
+    private void CountToALargeNumber(int id, int value)
+    {
+      for (var i = 0; i < value; i++)
+      {
+        // do nothing
+      }
+
+      Console.WriteLine($"Complete Counting {id} :\t{sw.Elapsed.TotalMilliseconds,10:N0} ms");
+    }
+  }
+}
--- src/learning-csharp-test/src/Books/ICS7/_21._01._01/T01.cs
+++ src/learning-csharp-test/src/Books/ICS7/_21._01._01/T01.cs
@@ -1,35 +1,34 @@
-using Luotao.Lib.Books.ICS7._21._01._01;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Luotao.Test.Books.ICS7._21._01._01
 {
-    /// <summary>
-    ///     21.1.1 a starting example
-    /// </summary>
-    public class T01
+  /// <summary>
+  ///     21.1.1 a starting example
+  /// </summary>
+  public class T01
+  {
+    private readonly ITestOutputHelper testOutputHelper;
+
+    public T01(ITestOutputHelper testOutputHelper)
     {
-        private readonly ITestOutputHelper testOutputHelper;
-
-        public T01(ITestOutputHelper testOutputHelper)
-        {
-            this.testOutputHelper = testOutputHelper;
-        }
-
-        [Fact]
-        public void _01()
-        {
-            var myDownloadString = new MyDownloadString(testOutputHelper.WriteLine);
-            myDownloadString.DoRun();
-        }
-
-        /// <summary>
-        /// </summary>
-        [Fact]
-        public void _02()
-        {
-            var myDownloadString = new MyDownloadStringAsync(testOutputHelper.WriteLine);
-            myDownloadString.DoRun();
-        }
+      this.testOutputHelper = testOutputHelper;
     }
+
+    [Fact]
+    public void _01()
+    {
+      var myDownloadString = new MyDownloadString();
+      myDownloadString.DoRun();
+    }
+
+    /// <summary>
+    /// </summary>
+    [Fact]
+    public void _02()
+    {
+      var myDownloadString = new MyDownloadStringAsync();
+      myDownloadString.DoRun();
+    }
+  }
 }
