3d6f1cd971ae4d04a78c4fba9f6efa1a08dbbbe6
luotao <luotaoyeah@gmail.com>
2020-06-07 22:25:15 +0800

    feat(learning-csharp-blazor): https://docs.microsoft.com/en-us/aspnet/core/blazor/fundamentals/routing?view=aspnetcore-5.0#route-parameters

---
+++ src/learning-csharp-blazor/Pages/Docs/Fundamentals/Routing/RouteParameters.razor.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+namespace Luotao.Blazor.Pages.Docs.Fundamentals.Routing
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/fundamentals/routing?view=aspnetcore-5.0#route-parameters.
+    /// </summary>
+    [Route("/docs/fundamentals/routing/route-parameters/{Name}/{Text?}")]
+    public partial class RouteParameters : ComponentBase
+    {
+        /// <summary>
+        /// 路径参数.
+        /// </summary>
+        [Parameter]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 可选的路径参数.
+        /// </summary>
+        [Parameter]
+        public string? Text { get; set; }
+
+        [Inject]
+        private ILogger<RouteParameters> Logger { get; set; }
+
+        /// <inheritdoc />
+        protected override void OnInitialized()
+        {
+            Logger.LogInformation($"OnInitialized().Text: {Text}");
+        }
+
+        /// <inheritdoc />
+        protected override void OnParametersSet()
+        {
+            Logger.LogInformation($"OnParametersSet().Text: {Text}");
+        }
+
+        /// <inheritdoc />
+        protected override void OnAfterRender(bool firstRender)
+        {
+            Logger.LogInformation($"OnAfterRender().firstRender: {firstRender.ToString()}");
+        }
+    }
+}
