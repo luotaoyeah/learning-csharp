a19130fcd6f0adaa88c621d011e7ce951fa4e0e7
luotao <luotaoyeah@gmail.com>
2020-06-11 20:34:59 +0800

    feat(learning-csharp-blazor): https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#child-content-1

--- src/learning-csharp-blazor/Pages/Docs/Components/Overview/ChildContent/ChildContent.razor.cs
+++ src/learning-csharp-blazor/Pages/Docs/Components/Overview/ChildContent/ChildContent.razor.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components;
 namespace Luotao.Blazor.Pages.Docs.Components.Overview.ChildContent
 {
     /// <summary>
-    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#child-content.
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#child-content-1.
     /// </summary>
     [Route("/docs/components/child-content")]
     public partial class ChildContent
--- src/learning-csharp-blazor/Pages/Docs/Components/Overview/ChildContent/RenderFragmentChild.razor.cs
+++ src/learning-csharp-blazor/Pages/Docs/Components/Overview/ChildContent/RenderFragmentChild.razor.cs
@@ -8,9 +8,16 @@ namespace Luotao.Blazor.Pages.Docs.Components.Overview.ChildContent
     public partial class RenderFragmentChild
     {
         /// <summary>
-        /// 按照约定, <see cref="RenderFragment"/> 类型的参数必须名为 ChildContent.
+        /// 如果只有一个 RenderFragment 类型的参数,则按照约定属性必须名为 ChildContent.
+        /// 这样一来, 在父组件的模板中子组件标签中间的所有内容都会被赋值到这个参数.
         /// </summary>
         [Parameter]
         public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+        public RenderFragment? Header { get; set; }
+
+        [Parameter]
+        public RenderFragment? Footer { get; set; }
     }
 }
