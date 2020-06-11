19d22766f03cbecebe4bfe4a59f0b797a11cc5fd
luotao <luotaoyeah@gmail.com>
2020-06-11 20:07:03 +0800

    feat(learning-csharp-blazor): https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#route-parameters-1

--- src/learning-csharp-blazor/Pages/Docs/Components/Overview/RouteParameters/RouteParameters.razor.cs
+++ src/learning-csharp-blazor/Pages/Docs/Components/Overview/RouteParameters/RouteParameters.razor.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNetCore.Components;
 namespace Luotao.Blazor.Pages.Docs.Components.Overview.RouteParameters
 {
     /// <summary>
-    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#route-parameters.
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#route-parameters-1.
     /// </summary>
-    [Route("/docs/components/route-parameters/{Name}/{Age:int}")]
+    [Route("/docs/components/route-parameters/{Name}/{Age:int?}")]
     public partial class RouteParameters
     {
         /// <summary>
@@ -18,6 +18,6 @@ namespace Luotao.Blazor.Pages.Docs.Components.Overview.RouteParameters
         /// 年龄.
         /// </summary>
         [Parameter]
-        public int Age { get; set; }
+        public int? Age { get; set; }
     }
 }
