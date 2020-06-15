2ae7f2d71418254603af07879cf68da265369ea3
luotao <luotaoyeah@gmail.com>
2020-06-15 13:43:08 +0800

    feat(learning-csharp-blazor): https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#razor-templates-1

--- src/learning-csharp-blazor/Pages/Docs/Components/Overview/RazorTemplates/RazorTemplates.razor.cs
+++ src/learning-csharp-blazor/Pages/Docs/Components/Overview/RazorTemplates/RazorTemplates.razor.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components;
 namespace Luotao.Blazor.Pages.Docs.Components.Overview.RazorTemplates
 {
     /// <summary>
-    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#razor-templates.
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#razor-templates-1.
     /// </summary>
     [Route("/docs/components/razor-templates")]
     public sealed partial class RazorTemplates
