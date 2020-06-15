dd53b8c7902fa88dfa617d27def198d50fe0b080
luotao <luotaoyeah@gmail.com>
2020-06-15 21:21:24 +0800

    feat(learning-csharp-blazor): https://docs.microsoft.com/en-us/aspnet/core/blazor/components/cascading-values-and-parameters?view=aspnetcore-5.0#cascadingvalue-component-1

--- src/learning-csharp-blazor/Pages/Docs/Components/CascadingValuesAndParameters/CascadingValueComponent/CascadingValueComponent.razor.cs
+++ src/learning-csharp-blazor/Pages/Docs/Components/CascadingValuesAndParameters/CascadingValueComponent/CascadingValueComponent.razor.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components;
 namespace Luotao.Blazor.Pages.Docs.Components.CascadingValuesAndParameters.CascadingValueComponent
 {
     /// <summary>
-    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/cascading-values-and-parameters?view=aspnetcore-5.0#cascadingvalue-component.
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/cascading-values-and-parameters?view=aspnetcore-5.0#cascadingvalue-component-1.
     /// </summary>
     [Route("/docs/components/cascading-values-and-parameters/cascadingvalue-component")]
     public partial class CascadingValueComponent
--- src/learning-csharp-blazor/Pages/Docs/Components/CascadingValuesAndParameters/CascadingValueComponent/Descendant.razor.cs
+++ src/learning-csharp-blazor/Pages/Docs/Components/CascadingValuesAndParameters/CascadingValueComponent/Descendant.razor.cs
@@ -8,7 +8,8 @@ namespace Luotao.Blazor.Pages.Docs.Components.CascadingValuesAndParameters.Casca
     public partial class Descendant
     {
         /// <summary>
-        /// ThemeInfo.
+        /// 默认情况下 <see cref="CascadingParameterAttribute"/> 是根据类型来匹配 <see cref="CascadingValue{TValue}"/> 的.
+        /// 如果多个 <see cref="CascadingValue{TValue}"/> 的类型相同, 则需要根据名称来匹配.
         /// </summary>
         [CascadingParameter]
         public ThemeInfo? ThemeInfo { get; set; }
