d37a6d41dec92cec8f6e13c07765edb13584cecd
luotao <luotaoyeah@gmail.com>
2020-06-15 15:24:12 +0800

    feat(learning-csharp-blazor): https://docs.microsoft.com/en-us/aspnet/core/blazor/components/layouts?view=aspnetcore-5.0#layout-components-1

---
+++ src/learning-csharp-blazor/Pages/Docs/Components/Layouts/LayoutComponents/LayoutComponents.razor.cs
@@ -0,0 +1,14 @@
+using Luotao.Blazor.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace Luotao.Blazor.Pages.Docs.Components.Layouts.LayoutComponents
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/layouts?view=aspnetcore-5.0#layout-components-1.
+    /// </summary>
+    [Layout(typeof(Layout01))]
+    [Route("/docs/components/layouts/create-a-layout-component")]
+    public partial class LayoutComponents
+    {
+    }
+}
