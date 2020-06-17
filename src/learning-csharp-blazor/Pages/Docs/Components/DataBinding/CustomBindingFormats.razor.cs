30f260d50e18d8e1b2f62117755e179ce8d061dd
luotao <luotaoyeah@gmail.com>
2020-06-17 20:22:27 +0800

    feat(learning-csharp-blazor): https://docs.microsoft.com/en-us/aspnet/core/blazor/components/data-binding?view=aspnetcore-5.0#custom-binding-formats-1

--- src/learning-csharp-blazor/Pages/Docs/Components/DataBinding/CustomBindingFormats.razor.cs
+++ src/learning-csharp-blazor/Pages/Docs/Components/DataBinding/CustomBindingFormats.razor.cs
@@ -3,13 +3,16 @@ using Microsoft.AspNetCore.Components;
 namespace Luotao.Blazor.Pages.Docs.Components.DataBinding
 {
     /// <summary>
-    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/data-binding?view=aspnetcore-5.0#custom-binding-formats.
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/data-binding?view=aspnetcore-5.0#custom-binding-formats-1.
     /// </summary>
     [Route("/docs/components/data-binding/custom-binding-formats")]
     public partial class CustomBindingFormats
     {
         private string? inputValue;
 
+        /// <summary>
+        /// 双向绑定一个 property 时, 可以在 get/set 里面对数据进行处理.
+        /// </summary>
         private string? InputValue
         {
             get => inputValue;
