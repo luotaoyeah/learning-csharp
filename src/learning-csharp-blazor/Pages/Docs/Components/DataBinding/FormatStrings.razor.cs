a07ab62551164f9b45066f9c554068d2b9b138a5
luotao <luotaoyeah@gmail.com>
2020-06-17 20:10:59 +0800

    feat(learning-csharp-blazor): https://docs.microsoft.com/en-us/aspnet/core/blazor/components/data-binding?view=aspnetcore-5.0#format-strings-1

--- src/learning-csharp-blazor/Pages/Docs/Components/DataBinding/FormatStrings.razor.cs
+++ src/learning-csharp-blazor/Pages/Docs/Components/DataBinding/FormatStrings.razor.cs
@@ -4,7 +4,7 @@ using System;
 namespace Luotao.Blazor.Pages.Docs.Components.DataBinding
 {
     /// <summary>
-    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/data-binding?view=aspnetcore-5.0#format-strings.
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/data-binding?view=aspnetcore-5.0#format-strings-1.
     /// </summary>
     [Route("/docs/components/data-binding/format-strings")]
     public partial class FormatStrings
