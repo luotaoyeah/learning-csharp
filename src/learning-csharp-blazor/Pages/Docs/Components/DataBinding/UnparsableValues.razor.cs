2f23a69524eea205506ead8cc34c6d6262e08a98
luotao <luotaoyeah@gmail.com>
2020-06-17 20:04:13 +0800

    feat(learning-csharp-blazor): https://docs.microsoft.com/en-us/aspnet/core/blazor/components/data-binding?view=aspnetcore-5.0#unparsable-values-1

--- src/learning-csharp-blazor/Pages/Docs/Components/DataBinding/UnparsableValues.razor.cs
+++ src/learning-csharp-blazor/Pages/Docs/Components/DataBinding/UnparsableValues.razor.cs
@@ -3,13 +3,14 @@ using Microsoft.AspNetCore.Components;
 namespace Luotao.Blazor.Pages.Docs.Components.DataBinding
 {
     /// <summary>
-    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/data-binding?view=aspnetcore-5.0#unparsable-values.
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/data-binding?view=aspnetcore-5.0#unparsable-values-1.
     /// </summary>
     [Route("/docs/components/data-binding/unparsable-values")]
     public partial class UnparsableValues
     {
         /// <summary>
-        /// 如果输入的值无法被转换为字段的类型, 则输入的值会被拒绝.
+        /// 如果输入的数据无法被转换为绑定的属性的类型,
+        /// 则输入的值会被拒绝, 属性会恢复原来的值.
         /// </summary>
         private int? inputValue = 666;
     }
