cbf4b10387244c4a5e0be8b0be98d351128e0ba2
luotao <luotaoyeah@gmail.com>
2020-06-18 21:51:07 +0800

    feat(learning-csharp-blazor): https://docs.microsoft.com/en-us/aspnet/core/blazor/components/event-handling?view=aspnetcore-5.0#lambda-expressions-1

--- src/learning-csharp-blazor/Pages/Docs/Components/EventHandling/LambdaExpressions.razor.cs
+++ src/learning-csharp-blazor/Pages/Docs/Components/EventHandling/LambdaExpressions.razor.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Logging;
 
 namespace Luotao.Blazor.Pages.Docs.Components.EventHandling
 {
     /// <summary>
-    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/event-handling?view=aspnetcore-5.0#lambda-expressions.
+    /// https://docs.microsoft.com/en-us/aspnet/core/blazor/components/event-handling?view=aspnetcore-5.0#lambda-expressions-1.
     /// </summary>
     [Route("/docs/components/event-handling/lambda-expressions")]
     public sealed partial class LambdaExpressions
     {
+        [Inject]
+        private ILogger<LambdaExpressions> Logger { get; set; }
+
+        private void OnClick(MouseEventArgs args, int i)
+        {
+            Logger.LogInformation($"{i.ToString()}: [{args.ClientX.ToString()}, {args.ClientY.ToString()}]");
+        }
     }
 }
