0e173fc307e8e2d45ffd3598c71959477d268b54
luotao <luotaoyeah@gmail.com>
2020-06-29 19:15:16 +0800

    feat(CS10NET6): 2.2.14 revealing the extent of the c# vocabulary

---
+++ src/learning-csharp-test/src/Books/CS10NET6/_02._02._14/T01.cs
@@ -0,0 +1,42 @@
+using System.Reflection;
+
+namespace Luotao.Test.Books.CS10NET6._02._02._14
+{
+    /// <summary>
+    /// 2.2.14 revealing the extent of the c# vocabulary
+    /// </summary>
+    public class T01
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public T01(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void _01()
+        {
+            Assembly? entryAssembly = Assembly.GetEntryAssembly();
+            if (entryAssembly == null)
+            {
+                return;
+            }
+
+            AssemblyName[] referencedAssemblies = entryAssembly.GetReferencedAssemblies();
+            foreach (AssemblyName name in referencedAssemblies)
+            {
+                Assembly assembly = Assembly.Load(name);
+                Type[] types = assembly.GetTypes();
+
+                int methodCount = 0;
+                foreach (Type type in types)
+                {
+                    methodCount += type.GetMethods().Length;
+                }
+
+                testOutputHelper.WriteLine($"{name.Name}:\n{assembly.GetTypes().Length,10:N0} Type\n{methodCount,10:N0} Method\n");
+            }
+        }
+    }
+}
