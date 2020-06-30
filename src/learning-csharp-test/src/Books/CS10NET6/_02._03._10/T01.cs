6a2ab22803815d6b38529baf14429e9c6809ed43
luotao <luotaoyeah@gmail.com>
2020-06-30 21:08:43 +0800

    feat(CS10NET6): 2.3.10. getting default values for types

--- src/learning-csharp-test/src/Books/CS10NET6/_02._03._10/T01.cs
+++ src/learning-csharp-test/src/Books/CS10NET6/_02._03._10/T01.cs
@@ -1,7 +1,7 @@
-namespace Luotao.Test.Books.CS10NET6._02._03._09
+namespace Luotao.Test.Books.CS10NET6._02._03._10
 {
     /// <summary>
-    ///     2.3.9 getting default values for types
+    /// 2.3.10. getting default values for types
     /// </summary>
     public class T01
     {
@@ -13,30 +13,42 @@ namespace Luotao.Test.Books.CS10NET6._02._03._09
         }
 
         /// <summary>
-        ///     使用 default() 这个 operator 获取某个类型的默认值，
-        ///     reference type 的默认值都是 null，
+        /// 使用 default() 这个 operator 获取某个类型的默认值.
         /// </summary>
         [Fact]
         public void _01()
         {
+            Assert.Equal(0, default(int));
+            Assert.Equal(0D, default(double));
+
+            // bool 类型的默认值为 false.
+            Assert.False(default(bool));
+
+            // reference 类型的默认值都是 null
             Assert.Null(default(string));
-            Assert.Equal(0, default);
-            Assert.False(default);
 
             //----------------------------------------------------------------------------------------------------
-            // DateTime 类型的默认值如下，
+            // DateTime 类型的默认值如下
             //----------------------------------------------------------------------------------------------------
             Assert.Equal("0001-01-01 00:00:00", default(DateTime).ToString("yyyy-MM-dd HH:mm:ss"));
         }
 
         /// <summary>
-        ///     如果可以推断 default() 中的类型，则可以省略类型，直接写作 default，
+        /// 如果可以推断 default() 中的类型, 则可以省略类型, 直接写作 default.
         /// </summary>
         [Fact]
         public void _02()
         {
             Assert.Equal(0, default);
+            Assert.Equal(0, default(int));
+
             Assert.False(default);
+            Assert.False(default(bool));
+
+            double d01 = 1D;
+            Assert.Equal(1D, d01);
+            d01 = default;
+            Assert.Equal(0D, d01);
         }
     }
 }
