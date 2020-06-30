0dfa3f788b15b2d1afb7faf3b24288bce8d110ff
luotao <luotaoyeah@gmail.com>
2020-06-30 22:35:12 +0800

    feat(CS10NET6): 2.4.3. simplifying the usage of the console

---
+++ src/learning-csharp-test/src/Books/CS10NET6/_02._04._03/T01.cs
@@ -0,0 +1,19 @@
+using static System.Console;
+
+namespace Luotao.Test.Books.CS10NET6._02._04._03
+{
+    /// <summary>
+    /// 2.4.3. simplifying the usage of the console
+    /// </summary>
+    public class T01
+    {
+        /// <summary>
+        /// 使用 using static 引入 static class, 然后直接使用里面的静态成员.
+        /// </summary>
+        [Fact]
+        public void _01()
+        {
+            WriteLine("666");
+        }
+    }
+}
