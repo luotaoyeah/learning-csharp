c7aa0db0eefd0acdd60166d1e6b8a8ddc1d33abb
luotao <luotaoyeah@gmail.com>
2020-06-30 20:52:22 +0800

    feat(CS10NET6): 2.3.9.3. using target-typed new to instantiate objects

--- src/learning-csharp-test/src/Books/CS10NET6/_02._03._08._02/T01.cs
+++ src/learning-csharp-test/src/Books/CS10NET6/_02._03._08._02/T01.cs
@@ -3,7 +3,7 @@ using Luotao.Lib.Books.CS10NET6._02._03._08._02;
 namespace Luotao.Test.Books.CS10NET6._02._03._08._02
 {
     /// <summary>
-    ///     2.3.8.2 using target-typed new to instantiate objects
+    /// 2.3.9.3. using target-typed new to instantiate objects
     /// </summary>
     public class T01
     {
@@ -15,14 +15,16 @@ namespace Luotao.Test.Books.CS10NET6._02._03._08._02
         }
 
         /// <summary>
-        ///     target-typed new 是 C# 9 中的一个新的语法，
-        ///     在使用 new 创建对象实例时，如果变量前面声明了类型，则可以省略 new 后面的类型，直接写作 new();
+        /// target-typed new 是 C# 9 中的一个新的语法,
+        /// 使用 new 调用构造函数创建实例对象时, 如果可以推断类型, 则可以省略 new 后面的类名, 直接写作 new()
         /// </summary>
         [Fact]
         public void _01()
         {
+            // 无法推断类型, 不能省略类名
             var xmlDocument01 = new XmlDocument();
 
+            // 可以推断类型, 可以省略类名
             XmlDocument xmlDocument02 = new();
         }
     }
