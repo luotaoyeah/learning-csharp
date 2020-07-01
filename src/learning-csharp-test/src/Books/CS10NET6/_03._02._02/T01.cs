3731fd4390337e4f96741da72bc0ec464966e8dd
luotao <luotaoyeah@gmail.com>
2020-07-01 21:14:14 +0800

    feat(CS10NET6): 3.2.2 pattern matching with the if statement

--- src/learning-csharp-test/src/Books/CS10NET6/_03._02._02/T01.cs
+++ src/learning-csharp-test/src/Books/CS10NET6/_03._02._02/T01.cs
@@ -1,7 +1,7 @@
-namespace Luotao.Test.Books.CS10NET6._03._02._03
+namespace Luotao.Test.Books.CS10NET6._03._02._02
 {
     /// <summary>
-    ///     3.2.3 pattern matching with the if statement
+    /// 3.2.2 pattern matching with the if statement
     /// </summary>
     public class T01
     {
@@ -13,18 +13,22 @@ namespace Luotao.Test.Books.CS10NET6._03._02._03
         }
 
         /// <summary>
-        ///     if 的条件中可以使用模式匹配(pattern matching)
+        /// if 的条件中可以使用模式匹配(pattern matching).
         /// </summary>
         [Fact]
         public void _01()
         {
-            object i = 3;
+            object o = 3;
 
-            // 如果 i 可以转换为 int 类型, 则将转换结果存到变量 j 中
-            if (i is int j)
-                testOutputHelper.WriteLine("YES:{0}", j);
+            // 如果 o 可以转换为 int 类型, 则将转换结果存到变量 i 中
+            if (o is int i)
+            {
+                testOutputHelper.WriteLine("YES: {0}", i);
+            }
             else
+            {
                 testOutputHelper.WriteLine("NO");
+            }
         }
     }
 }
