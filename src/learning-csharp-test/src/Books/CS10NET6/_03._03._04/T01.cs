8e0948fade8adb6668c71104a64c6f7dc61fe6e8
luotao <luotaoyeah@gmail.com>
2020-07-01 22:53:15 +0800

    feat(CS10NET6): 3.3.4 looping with the foreach statement

--- src/learning-csharp-test/src/Books/CS10NET6/_03._03._04/T01.cs
+++ src/learning-csharp-test/src/Books/CS10NET6/_03._03._04/T01.cs
@@ -3,7 +3,7 @@ using System.Collections;
 namespace Luotao.Test.Books.CS10NET6._03._03._04
 {
     /// <summary>
-    ///     3.3.4 looping with the foreach statement
+    /// 3.3.4 looping with the foreach statement
     /// </summary>
     public class T01
     {
@@ -15,17 +15,21 @@ namespace Luotao.Test.Books.CS10NET6._03._03._04
         }
 
         /// <summary>
+        /// foreach...in 遍历一个集合.
         /// </summary>
         [Fact]
         public void _01()
         {
             string[] names = { "A", "B", "C" };
 
-            foreach (string name in names) testOutputHelper.WriteLine(name);
+            foreach (string name in names)
+            {
+                testOutputHelper.WriteLine(name);
+            }
         }
 
         /// <summary>
-        ///     foreach 实际上是下面这段代码的语法糖.
+        /// foreach 实际上是下面这段代码的语法糖.
         /// </summary>
         [Fact]
         public void _02()
@@ -35,8 +39,12 @@ namespace Luotao.Test.Books.CS10NET6._03._03._04
             IEnumerator enumerator = names.GetEnumerator();
 
             while (enumerator.MoveNext())
+            {
                 if (enumerator.Current is string current)
+                {
                     testOutputHelper.WriteLine(current);
+                }
+            }
         }
     }
 }
