965b2e6fb6a535f8fac607730f5656ea1a087b4a
luotao <luotaoyeah@gmail.com>
2020-07-03 20:08:34 +0800

    feat(CS10NET6): 3.4.7. parsing from strings to numbers or dates and times

--- src/learning-csharp-test/src/Books/CS10NET6/_03._04._07/T01.cs
+++ src/learning-csharp-test/src/Books/CS10NET6/_03._04._07/T01.cs
@@ -1,51 +1,48 @@
 namespace Luotao.Test.Books.CS10NET6._03._04._07
 {
     /// <summary>
-    ///     3.4.7 handling exceptions when converting types
+    /// 3.4.7. parsing from strings to numbers or dates and times
     /// </summary>
     public class T01
     {
-        private readonly ITestOutputHelper testOutputHelper;
-
-        public T01(ITestOutputHelper testOutputHelper)
-        {
-            this.testOutputHelper = testOutputHelper;
-        }
-
         /// <summary>
-        ///     catch 后面可以不指定异常类型.
+        /// 将字符串转换为数字.
         /// </summary>
         [Fact]
         public void _01()
         {
-            try
-            {
-                var i01 = int.Parse("A");
-            }
-            catch
-            {
-                testOutputHelper.WriteLine("CATCH A EXCEPTION");
-            }
+            int i01 = int.Parse("6");
+            Assert.Equal(6, i01);
         }
 
         /// <summary>
-        ///     catch 后面可以指定异常类型.
+        /// <see cref="System.Int32.Parse(string)" /> 方法转换失败时会抛出异常.
         /// </summary>
         [Fact]
         public void _02()
         {
-            try
-            {
-                var i01 = int.Parse("A");
-            }
-            catch (FormatException e)
-            {
-                testOutputHelper.WriteLine(e.Message);
-            }
-            catch (Exception e)
-            {
-                testOutputHelper.WriteLine(e.Message);
-            }
+            Assert.Throws<FormatException>(
+                () =>
+                {
+                    var i01 = int.Parse("A");
+                }
+            );
+        }
+
+        /// <summary>
+        /// <see cref="System.Int32.TryParse(string?, out int)" /> 方法转换失败时会返回 false,
+        /// 转化成功时会返回 true, 并将转换结果存到 out 参数中.
+        /// </summary>
+        [Fact]
+        public void _03()
+        {
+            int i01;
+
+            Assert.False(int.TryParse("A", out i01));
+            Assert.Equal(default, i01);
+
+            Assert.True(int.TryParse("6", out i01));
+            Assert.Equal(6, i01);
         }
     }
 }
