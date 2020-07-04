602f12a996aea2961db1a01c311de40b438bd36b
luotao <luotaoyeah@gmail.com>
2020-07-04 23:29:24 +0800

    feat(CS10NET6): 5.3.4. storing a value using an enum type

--- src/learning-csharp-lib/src/Books/CS10NET6/_05/WondersOfTheAncientWorld.cs
+++ src/learning-csharp-lib/src/Books/CS10NET6/_05/WondersOfTheAncientWorld.cs
@@ -1,13 +1,13 @@
-namespace Luotao.Lib.Books.CS10NET6._05
+namespace Luotao.Lib.Books.CS10NET6._05;
+
+public enum WondersOfTheAncientWorld
 {
-    public enum WondersOfTheAncientWorld
-    {
-        A = 0,
-        B,
-        C,
-        D,
-        E,
-        F,
-        G
-    }
+    // 枚举元素的值默认是从 0 开始的, 依次递增
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G
 }
--- src/learning-csharp-test/src/Books/CS10NET6/_05._03._04/T01.cs
+++ src/learning-csharp-test/src/Books/CS10NET6/_05._03._04/T01.cs
@@ -1,43 +1,37 @@
 using Luotao.Lib.Books.CS10NET6._05;
 
-namespace Luotao.Test.Books.CS10NET6._05._03._04
+namespace Luotao.Test.Books.CS10NET6._05._03._04;
+
+/// <summary>
+/// 5.3.4. storing a value using an enum type
+/// </summary>
+public class T01
 {
     /// <summary>
-    ///     5.3.4 storing a value using an enum type
+    /// enum 的元素默认是 int 类型, 因此 enum 是值类型, 它的默认值为 0.
     /// </summary>
-    public class T01
+    [Fact]
+    public void _01()
     {
-        private readonly ITestOutputHelper testOutputHelper;
+        var person = new Person();
+        Assert.Equal(0, (int)person.Favorite);
+        Assert.Equal(0, (int)default(WondersOfTheAncientWorld));
+    }
 
-        public T01(ITestOutputHelper testOutputHelper)
-        {
-            this.testOutputHelper = testOutputHelper;
-        }
+    /// <summary>
+    /// 可以将 int 值转换为 enum 值, 即使该 int 值无法真正对应该 enum 的任何元素.
+    /// </summary>
+    [Fact]
+    public void _02()
+    {
+        var person = new Person();
 
-        /// <summary>
-        ///     enum 底层存的是 int 值, 因此 enum 是值类型, 它的默认值为 0.
-        /// </summary>
-        [Fact]
-        public void _01()
-        {
-            var person = new Person();
-            Assert.Equal(0, (int)person.Favorite);
-            Assert.Equal(0, (int)default(WondersOfTheAncientWorld));
-        }
+        // 3 对应了 WondersOfTheAncientWorld.D
+        person.Favorite = (WondersOfTheAncientWorld)3;
+        Assert.Equal("D", person.Favorite.ToString());
 
-        /// <summary>
-        ///     可以将 int 值转换为 enum 值.
-        /// </summary>
-        [Fact]
-        public void _02()
-        {
-            var person = new Person();
-
-            person.Favorite = (WondersOfTheAncientWorld)3;
-            Assert.Equal("D", person.Favorite.ToString());
-
-            person.Favorite = (WondersOfTheAncientWorld)9;
-            Assert.Equal("9", person.Favorite.ToString());
-        }
+        // 9 无法对应任何枚举元素
+        person.Favorite = (WondersOfTheAncientWorld)9;
+        Assert.Equal("9", person.Favorite.ToString());
     }
 }
