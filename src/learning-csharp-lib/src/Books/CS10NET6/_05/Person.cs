116f101bb043b28aeb831c221a2978b7a5dc15b1
luotao <luotaoyeah@gmail.com>
2020-07-05 13:51:44 +0800

    feat(CS10NET6): 5.4.3. making a field constant

--- src/learning-csharp-lib/src/Books/CS10NET6/_05/Person.cs
+++ src/learning-csharp-lib/src/Books/CS10NET6/_05/Person.cs
@@ -5,6 +5,11 @@ namespace Luotao.Lib.Books.CS10NET6._05;
 
 public class Person
 {
+    /// <summary>
+    /// const field 隐式就是 static 的, 因此 static 和 const 不能同时使用.
+    /// </summary>
+    public const string Species = "Homo Sapiens";
+
     public List<Person> Children = new();
     public DateTime DateOfBirth;
     public WondersOfTheAncientWorld Favorite;
---
+++ src/learning-csharp-test/src/Books/CS10NET6/_05._04._03/T01.cs
@@ -0,0 +1,16 @@
+using Luotao.Lib.Books.CS10NET6._05;
+
+namespace Luotao.Test.Books.CS10NET6._05._04._03;
+
+/// <summary>
+/// 5.4.3. making a field constant
+/// </summary>
+public class T01
+{
+    [Fact]
+    public void _01()
+    {
+        // const field 必须通过类名访问, 因为 const field 就是 static 的
+        Assert.Equal("Homo Sapiens", Person.Species);
+    }
+}
