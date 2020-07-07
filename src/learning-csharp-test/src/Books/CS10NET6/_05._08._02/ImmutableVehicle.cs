8daf3ee9cfd02bcc109086e6a3e9472fb9896689
luotao <luotaoyeah@gmail.com>
2020-07-07 20:05:08 +0800

    feat(CS10NET6): 5.8.2. understanding records

---
+++ src/learning-csharp-test/src/Books/CS10NET6/_05._08._02/ImmutableVehicle.cs
@@ -0,0 +1,14 @@
+namespace Luotao.Test.Books.CS10NET6._05._08._02;
+
+/// <summary>
+/// record 是 reference 类型(record class), record struct 是 value 类型.
+/// </summary>
+public record ImmutableVehicle
+{
+    public string? Color { get; init; }
+
+    /// <summary>
+    /// record 的 property 不一定非得是 init-only property.
+    /// </summary>
+    public int Wheels { get; set; }
+}
---
+++ src/learning-csharp-test/src/Books/CS10NET6/_05._08._02/T01.cs
@@ -0,0 +1,21 @@
+namespace Luotao.Test.Books.CS10NET6._05._08._02;
+
+/// <summary>
+/// 5.8.2. understanding records
+/// </summary>
+public class T01
+{
+    [Fact]
+    public void _01()
+    {
+        var car01 = new ImmutableVehicle
+        {
+            Color = "Red",
+            Wheels = 4,
+        };
+
+        // 使用 with 在不修改原对象的基础上, 创建一个新的对象, 并更改某些属性的值
+        var car02 = car01 with { Color = "Blue" };
+        Assert.Equal("Blue", car02.Color);
+    }
+}
