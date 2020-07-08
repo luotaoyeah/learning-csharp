bbb0757c5e8c48d9cd7498864aa5ad75e4f84805
luotao <luotaoyeah@gmail.com>
2020-07-08 20:55:27 +0800

    feat(CS10NET6): 6.3.1. calling methods using delegates

---
+++ src/learning-csharp-test/src/Books/CS10NET6/_06._03._01/Delegate01.cs
@@ -0,0 +1,6 @@
+namespace Luotao.Test.Books.CS10NET6._06._03._01;
+
+/// <summary>
+/// 使用 delegate 关键字定义一个 delegate 类型, 实际上就定义了它的方法签名.
+/// </summary>
+public delegate int Delegate01(int x, int y);
