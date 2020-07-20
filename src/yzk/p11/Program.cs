21231e8d44546843d32021b79a21c4f3f7c4354f
luotao <luotaoyeah@gmail.com>
2020-07-20 20:38:36 +0800

    feat(yzk): https://www.bilibili.com/video/BV1pK41137He?p=11

--- src/yzk/p11/Program.cs
+++ src/yzk/p11/Program.cs
@@ -1,26 +1,41 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace P12;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        F01();
+        // F01();
+
+        await F02Async();
     }
 
+    /// <summary>
+    /// 同步方法.
+    /// </summary>
     private static void F01()
     {
-        Directory.CreateDirectory(@"D:/temp/p12/");
-
-        string path = @"D:/temp/p12/000.txt";
-
+        Directory.CreateDirectory(@"D:/temp/p11/");
+        string path = @"D:/temp/p11/000.txt";
         File.WriteAllText(path, "HELLO WORLD");
-
         string text = File.ReadAllText(path);
         Console.WriteLine(text);
+        Console.Read();
+    }
 
+    /// <summary>
+    /// 异步方法.
+    /// </summary>
+    private static async Task F02Async()
+    {
+        Directory.CreateDirectory(@"D:/temp/p11");
+        string path = @"D:/temp/p11/000.txt";
+        await File.WriteAllTextAsync(path, "HELLO WORLD");
+        string text = await File.ReadAllTextAsync(path);
+        Console.WriteLine(text);
         Console.Read();
     }
 }
