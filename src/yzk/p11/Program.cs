dc105b7aa612ebfd8e353c75653ced6a40f7cce2
luotao <luotaoyeah@gmail.com>
2020-07-20 20:28:36 +0800

    feat(yzk): https://www.bilibili.com/video/BV1pK41137He?p=11

---
+++ src/yzk/p11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace P12;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        F01();
+    }
+
+    private static void F01()
+    {
+        Directory.CreateDirectory(@"D:/temp/p12/");
+
+        string path = @"D:/temp/p12/000.txt";
+
+        File.WriteAllText(path, "HELLO WORLD");
+
+        string text = File.ReadAllText(path);
+        Console.WriteLine(text);
+
+        Console.Read();
+    }
+}
