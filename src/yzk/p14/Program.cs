77fbb9d015556deee615905606a898c91a0e5262
luotao <luotaoyeah@gmail.com>
2020-07-21 19:40:08 +0800

    feat(yzk): https://www.bilibili.com/video/BV1pK41137He?p=14
    Thread.CurrentThread.ManagedThreadId

---
+++ src/yzk/p14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P14;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine($"1: {Thread.CurrentThread.ManagedThreadId}");
+
+        await F01Async();
+
+        Console.WriteLine($"2: {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    private static async Task F01Async()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 1; i++)
+        {
+            stringBuilder.AppendLine("FOO");
+        }
+
+        Directory.CreateDirectory(@"D:/temp/p14");
+        await File.WriteAllTextAsync(@"D:/temp/p14/000.txt", stringBuilder.ToString());
+    }
+}
