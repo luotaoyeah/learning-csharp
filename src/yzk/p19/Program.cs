a501bf70b5a5488797bdf19327aaf1cf2a4f4f47
luotao <luotaoyeah@gmail.com>
2020-07-21 21:17:24 +0800

    feat(yzk): https://www.bilibili.com/video/BV1pK41137He?p=19
    Task.WhenAll()
    Task.WhenAny()

---
+++ src/yzk/p19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace P19;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        // await F01Async();
+
+        await F02Async();
+
+        Console.WriteLine(stopwatch.Elapsed.Seconds);
+    }
+
+    private static Task F01Async()
+    {
+        return Task.WhenAll(Task.Delay(1000), Task.Delay(2000), Task.Delay(3000));
+    }
+
+    private static Task F02Async()
+    {
+        return Task.WhenAny(Task.Delay(1000), Task.Delay(2000), Task.Delay(3000));
+    }
+}
