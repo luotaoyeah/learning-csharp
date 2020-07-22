d0a0070d5c05152e46f18f130213e60bf2b70908
luotao <luotaoyeah@gmail.com>
2020-07-22 21:11:48 +0800

    refactor(illustrated-c#-7): 14.8 removing methods from a delegate

--- src/learning-csharp-test/src/Books/ICS7/_14/_08/T01.cs
+++ src/learning-csharp-test/src/Books/ICS7/_14/_08/T01.cs
@@ -1,9 +1,7 @@
-using Luotao.Lib.Books.ICS7._14._08;
-
 namespace Luotao.Test.Books.ICS7._14._08
 {
     /// <summary>
-    ///     14.8 removing methods from a delegate
+    /// 14.8 removing methods from a delegate
     /// </summary>
     public class T01
     {
@@ -25,13 +23,13 @@ namespace Luotao.Test.Books.ICS7._14._08
         }
 
         /// <summary>
-        ///     使用 -= 从 delegate 对象中删除 method，
-        ///     从 invocation list 的最后面开始删除，如果同一个方法存在多次，则每次只会删除一次，
+        /// 使用 -= 从 delegate 中删除一个方法, 从 invocation list 的后面开始删除, 如果同一个方法存在多个, 则每次只会删除一个.
+        /// 跟添加一样, 删除方法也会创建一个新的 delegate 对象.
         /// </summary>
         [Fact]
         public void _01()
         {
-            var delegate01 = new MyDelegate(F01);
+            var delegate01 = new MyDel(F01);
             delegate01 += F02;
             delegate01 += F01;
             delegate01();
@@ -41,13 +39,13 @@ namespace Luotao.Test.Books.ICS7._14._08
             delegate01 -= F01;
             delegate01();
 
-            //----------------------------------------------------------------------------------------------------
-            // 当 invocation list 中所有的 method 都被删除之后，delegate 就为 null
-            //----------------------------------------------------------------------------------------------------
+            // 当所有的 method 都被删除之后, delegate 就为 null
             Assert.NotNull(delegate01);
             delegate01 -= F02;
             delegate01 -= F01;
             Assert.Null(delegate01);
         }
     }
+
+    internal delegate void MyDel();
 }
