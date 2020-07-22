e5608241e57e105d99d34812a8f8e50d29f8e396
luotao <luotaoyeah@gmail.com>
2020-07-22 20:06:39 +0800

    feat(yzk): https://www.bilibili.com/video/BV1pK41137He?p=20
    IAsyncEnumerable
    IAsyncEnumerator

---
+++ src/yzk/p20/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace P20;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await foreach (string s in F01Async())
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    /// <summary>
+    /// iterator 中使用 async 时, 返回类型需要使用 IAsyncEnumerator / IAsyncEnumerable,
+    /// 同时在遍历时需要使用 await foreach.
+    /// </summary>
+    /// <returns></returns>
+    private static async IAsyncEnumerable<string> F01Async()
+    {
+        Console.WriteLine("准备返回 A");
+        yield return await F02Async("A");
+
+        Console.WriteLine("准备返回 B");
+        yield return await F02Async("B");
+
+        Console.WriteLine("准备返回 C");
+        yield return await F02Async("C");
+    }
+
+    private static async Task<string> F02Async(string s)
+    {
+        await Task.Delay(1000);
+        return s;
+    }
+}
