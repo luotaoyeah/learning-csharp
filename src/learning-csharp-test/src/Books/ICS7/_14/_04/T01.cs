83909e7e9dcdfcb23f20582b1145f07837621c9f
luotao <luotaoyeah@gmail.com>
2020-07-22 20:45:39 +0800

    refactor(illustrated-c#-7): 14.4 creating the delegate object

--- src/learning-csharp-test/src/Books/ICS7/_14/_04/T01.cs
+++ src/learning-csharp-test/src/Books/ICS7/_14/_04/T01.cs
@@ -1,42 +1,34 @@
-using Luotao.Lib.Books.ICS7._14._04;
-
 namespace Luotao.Test.Books.ICS7._14._04
 {
     /// <summary>
-    ///     14.4 creating the delegate object
+    /// 14.4 creating the delegate object
     /// </summary>
     public class T01
     {
-        private readonly ITestOutputHelper testOutputHelper;
-
-        public T01(ITestOutputHelper testOutputHelper)
-        {
-            this.testOutputHelper = testOutputHelper;
-        }
-
         [Fact]
         public void _01()
         {
-            var c01 = new C01();
+            // 使用 new 创建一个 delegate 对象, 并且必须传入一个方法, 作为 invocation list 中的第一个方法.
+            MyDel del = new MyDel(new C01().F01);
 
-            //----------------------------------------------------------------------------------------------------
-            // 使用 object-creation expression 语句创建一个 delegate 对象，
-            // 必须传一个 method 作为参数，这个 method 会成为该 delegate 对象的 invocation list 中的第一个 method，
-            //----------------------------------------------------------------------------------------------------
-            MyDel myDel = c01.Fn01;
+            // 直接将一个方法赋值给 delegate 对象, 实际上是一个语法糖, 跟上面的含义一样.
+            del = C01.F02;
 
-            //----------------------------------------------------------------------------------------------------
-            // invocation list 中的 method 可以是 instance method 或者 static method，
-            // 只要 method 的 signature 和 return type 是匹配的，
-            //----------------------------------------------------------------------------------------------------
-            myDel = C01.Fn02;
+            // delegate 类型是引用类型.
+            Assert.NotNull(del);
+        }
+    }
 
-            //----------------------------------------------------------------------------------------------------
-            // 可以直接将一个 method 赋值给 delegate，会自动进行隐式类型转换
-            //----------------------------------------------------------------------------------------------------
-            myDel = c01.Fn01;
+    delegate void MyDel(int p);
 
-            Assert.Equal(4, myDel(2));
+    class C01
+    {
+        public void F01(int x)
+        {
+        }
+
+        public static void F02(int x)
+        {
         }
     }
 }
