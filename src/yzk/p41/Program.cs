85bfa02f10ac27a517ee15084339b410392dc8b9
luotao <luotaoyeah@gmail.com>
2020-07-25 19:37:43 +0800

    feat(yzk): https://www.bilibili.com/video/BV1pK41137He?p=41
    Microsoft.Extensions.Configuration.CommandLine
    Microsoft.Extensions.Configuration.EnvironmentVariables
    IConfigurationBuilder.AddCommandLine()
    IConfigurationBuilder.AddEnvironmentVariables()

---
+++ src/yzk/p41/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace P41;
+
+public class Program
+{
+    /// <summary>
+    /// dotnet 命令行如何传参数, 多级参数使用 : 分隔
+    ///   dotnet run Person:Name=luotao Person:Age=19
+    ///
+    /// </summary>
+    /// <param name="args"></param>
+    public static void Main(string[] args)
+    {
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        IConfigurationRoot configurationRoot = configurationBuilder
+            .AddCommandLine(args)
+            .AddEnvironmentVariables("LT_")
+            .Build();
+
+        Console.WriteLine(configurationRoot["Person:Name"]);
+        Console.WriteLine(configurationRoot.GetSection("Person:Age").Value);
+        Console.WriteLine(configurationRoot.GetSection("FOO").Value);
+    }
+}
