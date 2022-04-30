c2c78c00eb4310760114a49c423fbca3daea1acc
luotao <luotaoyeah@gmail.com>
2022-04-30 23:13:40 +0800

    feat(10.PROCN2021): 2.1.4. understanding variable scope

---
+++ projects/10/src/learning-csharp/src/Books/PROCN2021/_02/_01/_04/_01.cs
@@ -0,0 +1,26 @@
+namespace Luotao.LearningCsharp.Test.Books.PROCN2021._02._01._04;
+
+/// <summary>
+/// 2.1.4. understanding variable scope
+/// </summary>
+public class _04
+{
+    private readonly ITestOutputHelper testOutputHelper;
+
+    public _04(ITestOutputHelper testOutputHelper)
+    {
+        this.testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void _01()
+    {
+        {
+            // CS0136
+            // 同一个作用域内, 变量不能同名
+            // int i = 1;
+        }
+
+        int i = 2;
+    }
+}
