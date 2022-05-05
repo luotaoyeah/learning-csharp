08d50bb9d6f82072a54feba50126b5d07ab36fa8
luotao <luotaoyeah@gmail.com>
2022-05-05 20:34:45 +0800

    feat(10.PROCN2021): 3.3.4. methods

--- projects/10/src/learning-csharp/src/Books/PROCN2021/_03/_03/T03.cs
+++ projects/10/src/learning-csharp/src/Books/PROCN2021/_03/_03/T03.cs
@@ -1,40 +1,44 @@
-namespace Luotao.LearningCsharp.Test.Books.PROCN2021._03._03;
-
-/// <summary>
-/// 3.3.3. properties
-/// </summary>
-public class T03
+namespace Luotao.LearningCsharp.Test.Books.PROCN2021._03._03
 {
-    private readonly ITestOutputHelper testOutputHelper;
-
-    public T03(ITestOutputHelper testOutputHelper)
+    namespace _03
     {
-        this.testOutputHelper = testOutputHelper;
+        /// <summary>
+        /// 3.3.3. properties
+        /// </summary>
+        public class T03
+        {
+            private readonly ITestOutputHelper testOutputHelper;
+
+            public T03(ITestOutputHelper testOutputHelper)
+            {
+                this.testOutputHelper = testOutputHelper;
+            }
+
+            [Fact]
+            public void _01()
+            {
+            }
+        }
+
+        class A
+        {
+            private int age;
+
+            public int Age
+            {
+                get => age;
+                set => age = value;
+            }
+
+            public string? Name { get; set; } = "luotao";
+
+            public int X01
+            {
+                get => 0;
+            }
+
+            // expression-bodied property
+            public int X02 => 0;
+        }
     }
-
-    [Fact]
-    public void _01()
-    {
-    }
-}
-
-class A
-{
-    private int age;
-
-    public int Age
-    {
-        get => age;
-        set => age = value;
-    }
-
-    public string? Name { get; set; } = "luotao";
-
-    public int X01
-    {
-        get => 0;
-    }
-
-    // expression-bodied property
-    public int X02 => 0;
 }
---
+++ projects/10/src/learning-csharp/src/Books/PROCN2021/_03/_03/T04.cs
@@ -0,0 +1,66 @@
+namespace Luotao.LearningCsharp.Test.Books.PROCN2021._03._03
+{
+    namespace _04
+    {
+        /// <summary>
+        /// 3.3.4. methods
+        /// </summary>
+        public class T04
+        {
+            private readonly ITestOutputHelper testOutputHelper;
+
+            public T04(ITestOutputHelper testOutputHelper)
+            {
+                this.testOutputHelper = testOutputHelper;
+            }
+
+            [Fact]
+            public void _01()
+            {
+                // named arguments
+                Assert.Equal(7, A.Fn01(1, z: 4));
+            }
+
+            [Fact]
+            public void _02()
+            {
+                int[] arr01 = { 2, 3 };
+                Assert.Equal(6, A.Fn02(1, arr01));
+                Assert.Equal(6, A.Fn02(1, 2, 3));
+            }
+        }
+    }
+
+    class A
+    {
+        /// <summary>
+        /// optional arguments
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <returns></returns>
+        public static int Fn01(int x = 1, int y = 2, int z = 3)
+        {
+            return x + y + z;
+        }
+
+        /// <summary>
+        /// variable number of arguments
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int Fn02(int x, params int[] nums)
+        {
+            int sum = x;
+
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+
+            return sum;
+        }
+    }
+}
