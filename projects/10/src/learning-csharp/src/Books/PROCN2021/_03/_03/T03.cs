65ba06f4e4c9290e1d5e01428c75a1152f4bcc7b
luotao <luotaoyeah@gmail.com>
2022-05-05 19:59:46 +0800

    feat(10.PROCN2021): 3.3.3. properties

---
+++ projects/10/src/learning-csharp/src/Books/PROCN2021/_03/_03/T03.cs
@@ -0,0 +1,40 @@
+namespace Luotao.LearningCsharp.Test.Books.PROCN2021._03._03;
+
+/// <summary>
+/// 3.3.3. properties
+/// </summary>
+public class T03
+{
+    private readonly ITestOutputHelper testOutputHelper;
+
+    public T03(ITestOutputHelper testOutputHelper)
+    {
+        this.testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void _01()
+    {
+    }
+}
+
+class A
+{
+    private int age;
+
+    public int Age
+    {
+        get => age;
+        set => age = value;
+    }
+
+    public string? Name { get; set; } = "luotao";
+
+    public int X01
+    {
+        get => 0;
+    }
+
+    // expression-bodied property
+    public int X02 => 0;
+}
