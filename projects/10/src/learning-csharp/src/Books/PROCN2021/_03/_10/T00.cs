6254653744b5f2465bb5aeb7b7b8e98f930e3366
luotao <luotaoyeah@gmail.com>
2022-05-07 13:51:53 +0800

    feat(10.PROCN2021): 3.10. deconstruction

---
+++ projects/10/src/learning-csharp/src/Books/PROCN2021/_03/_10/T00.cs
@@ -0,0 +1,50 @@
+namespace Luotao.LearningCsharp.Test.Books.PROCN2021._03._10
+{
+    namespace _00
+    {
+        /// <summary>
+        /// 3.10. deconstruction
+        /// </summary>
+        public class T01
+        {
+            private readonly ITestOutputHelper testOutputHelper;
+
+            public T01(ITestOutputHelper testOutputHelper)
+            {
+                this.testOutputHelper = testOutputHelper;
+            }
+
+            [Fact]
+            public void _01()
+            {
+                Person person = new() { Name = "luotao", Age = 18 };
+
+                (string? name, int age) = person;
+                Assert.Equal("luotao", name);
+                Assert.Equal(18, age);
+            }
+        }
+
+        class Person
+        {
+            public string? Name { get; set; }
+            public int Age { get; set; }
+
+            public void Deconstruct(out string? name, out int age)
+            {
+                name = Name;
+                age = Age;
+            }
+
+            public void Deconstruct(out string? name)
+            {
+                name = Name;
+            }
+
+            public void Deconstruct(out int age)
+            {
+                age = Age;
+            }
+        }
+    }
+}
