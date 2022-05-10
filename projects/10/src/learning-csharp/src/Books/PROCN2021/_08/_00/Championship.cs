61ec6c9b81f6d232a5371ab4811907f48bf5b994
luotao <luotaoyeah@gmail.com>
2022-05-10 23:26:44 +0800

    feat(10.PROCN2021): 9.1.1. lists and entities

---
+++ projects/10/src/learning-csharp/src/Books/PROCN2021/_08/_00/Championship.cs
@@ -0,0 +1,3 @@
+﻿namespace Luotao.LearningCsharp.Test.Books.PROCN2021._08._00;
+
+public record Championship(int Year, string First, string Second, string Third);
---
+++ projects/10/src/learning-csharp/src/Books/PROCN2021/_08/_00/Team.cs
@@ -0,0 +1,13 @@
+﻿namespace Luotao.LearningCsharp.Test.Books.PROCN2021._08._00;
+
+public record Team
+{
+    public Team(string name, params int[] years)
+    {
+        Name = name;
+        Years = years is not null ? new List<int>(years) : new List<int>();
+    }
+
+    public string Name { get; }
+    public IEnumerable<int> Years { get; }
+}
